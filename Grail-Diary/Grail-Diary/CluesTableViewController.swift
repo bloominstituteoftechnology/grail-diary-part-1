0681831677c6f28fd36dace088ba888d66a9b93a
Marlon <marmoneymar@icloud.com>
2019-07-16 17:00:41 -0400

    Storyboard layout complete. Everything is setup and ready to go.

---
+++ Grail-Diary/Grail-Diary/CluesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CluesTableViewController.swift
+//  Grail-Diary
+//
+//  Created by Marlon Raskin on 7/16/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
