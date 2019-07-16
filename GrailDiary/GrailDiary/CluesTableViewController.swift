898924e10b5bdc99c87bd53f03a5f61937cb27ed
JerryHaaser <jerry.haaser@hotmail.com>
2019-07-16 15:01:53 -0500

    Renamed View Controller to CluesTableViewController, added in table view and embeded it to navigation Controller

---
+++ GrailDiary/GrailDiary/CluesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CluesTableViewController.swift
+//  GrailDiary
+//
+//  Created by admin on 7/16/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
