99a43aa35c76c620ecad79e92e88beb44f986e9f
Percy Ngan <percival.ngan@gmail.com>
2019-07-16 16:03:06 -0500

    Added 5 text fields to View Controller.

--- GrailDiary/GrailDiary/CluestTableViewController.swift
+++ GrailDiary/GrailDiary/CluestTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CluesTableViewController .swift
 //  GrailDiary
 //
 //  Created by Percy Ngan on 7/16/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CluesTableViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
