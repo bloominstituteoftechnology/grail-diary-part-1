1d4becff48a0609c6c6250eb2213c16b878857b5
Jeffrey Santana <SantanaJny@gmail.com>
2019-07-16 17:01:04 -0400

    Auto layout complete

---
+++ Grail Diary/Grail Diary/CluesTableVC.swift
@@ -0,0 +1,20 @@
+//
+//  CluesTableVC.swift
+//  Grail Diary
+//
+//  Created by Jeffrey Santana on 7/16/19.
+//  Copyright © 2019 Jefffrey Santana. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableVC: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
