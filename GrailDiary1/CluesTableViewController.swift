9ceb4b39af3c002be33caac7a60e8223095ce120
Taylor Lyles <hawks93187@gmail.com>
2019-07-16 15:51:05 -0700

    Set up Table view and added model segue to VC

---
+++ GrailDiary1/CluesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary1
+//
+//  Created by Taylor Lyles on 7/16/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
