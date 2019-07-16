f11e3cd750d7b2f248d0912ce7fdf86a133c3da7
shillwil <alex.shillingford@icloud.com>
2019-07-16 14:16:54 -0600

    First commit

---
+++ ios-8-grail-diary/ios-8-grail-diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-8-grail-diary
+//
+//  Created by Alex Shillingford on 7/16/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
