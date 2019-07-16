91e94df6472a98467ad180771a337ab70297769c
Alex Rhodes <anrhodes5738@gmail.com>
2019-07-16 14:06:54 -0500

    Added xcode file.

---
+++ GrailDiary/GrailDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Alex Rhodes on 7/16/19.
+//  Copyright © 2019 Alex Rhodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
