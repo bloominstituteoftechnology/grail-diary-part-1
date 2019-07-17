d3fc58ddc679e073f23691e31eba38eddb2eadf7
Marlon <marmoneymar@icloud.com>
2019-07-17 18:45:02 -0400

    Day 2 of Grail Diary is complete

---
+++ Grail-Diary/Grail-Diary/Models/POI.swift
@@ -0,0 +1,15 @@
+//
+//  POI.swift
+//  Grail-Diary
+//
+//  Created by Marlon Raskin on 7/17/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import Foundation
+
+struct POI: Equatable {
+	let location: String
+	let country: String
+	var clues: [String]
+}
