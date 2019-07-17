043fcd5504117fca9927121d67798d159469d8f9
ryandutson <mr.dutson@gmail.com>
2019-07-17 16:39:01 -0600

    Intital commit.

---
+++ GrailDiary/GrailDiary/Models/POI.swift
@@ -0,0 +1,14 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Ryan Dutson on 7/17/19.
+//  Copyright © 2019 Ryan Dutson. All rights reserved.
+//
+
+import Foundation
+
+struct POI
+let location: String
+let country:
+let clues: [String]
---
+++ GrailDiary/GrailDiary/View Controllers/POIsTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  POIsTableViewController.swift
+//  GrailDiary
+//
+//  Created by Ryan Dutson on 7/17/19.
+//  Copyright © 2019 Ryan Dutson. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+        let poiModelsArray: [POI] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    extension POIsTableViewController {
+        
+    }
+}
