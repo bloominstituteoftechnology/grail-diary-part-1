eac1c522e2a652faacf38538ab8dadb06e038142
Taylor Lyles <hawks93187@gmail.com>
2019-07-17 14:05:18 -0700

    Added VC's, MOel, and TVC

---
+++ GrailDiary1/New Group/POI.swift
@@ -0,0 +1,15 @@
+//
+//  POI.swift
+//  GrailDiary1
+//
+//  Created by Taylor Lyles on 7/17/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import Foundation
+
+struct POI {
+	let location: String
+	let country: String
+	let clues: [String]
+}
