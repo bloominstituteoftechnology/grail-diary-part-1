b486e917846ce3872f06b6c2c0029b82ff776350
Andrew Ruiz <nukedoom@gmail.com>
2019-07-17 20:29:04 -0700

    Implemented tableviewCell, deleted CluesTableViewController, and added POIsTableViewController extension.

--- Grail Diary Part 1/View Controllers/POIsTableViewController.swift
+++ Grail Diary Part 1/View Controllers/POIsTableViewController.swift
@@ -9,22 +9,30 @@
 import UIKit
 
 class POIsTableViewController: UIViewController {
-
+    
+    // Strange that as I'm typing out  @IBOutlet Xcode doesn't auto-complete. Almost as if Apple is saying, "Don't do this programmatically. Use the storyboard method." Maybe Apple has a preference for how things should be done. Or maybe it's just a bug.
+    @IBOutlet weak var tableView: UITableView!
+    
+    var pois : [POI] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension POIsTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pois.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath) as? POITableViewCell else {return UITableViewCell()}
+        
+        let poi = pois[indexPath.row]
+        cell.poi = poi
+        return cell
+    }
 }
