c4eb437ecf838204fd0056eab6c20d2adc41f871
Andrew Ruiz <nukedoom@gmail.com>
2019-07-17 20:54:18 -0700

    Added cancelTapped and saveTapped functions

--- Grail Diary Part 1/View Controllers/AddPOIViewController.swift
+++ Grail Diary Part 1/View Controllers/AddPOIViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+protocol AddPOIDelegate {
+    func poiWasAdded(_ poi: POI)
+}
+
 class AddPOIViewController: UIViewController {
 
     @IBOutlet weak var locationTextField: UITextField!
@@ -16,5 +20,36 @@ class AddPOIViewController: UIViewController {
     @IBOutlet weak var clue2TextField: UITextField!
     @IBOutlet weak var clue3TextField: UITextField!
     
+    var delegate: AddPOIDelegate?
+    
+    @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+        guard let location = locationTextField.text,
+            let country = countryTextField.text,
+            !location.isEmpty,
+            !country.isEmpty else {return}
+        
+        var poi = POI(location: location, country: country, clues: [])
+        
+        if let clue1 = clue1TextField.text,
+            !clue1.isEmpty {
+            poi.clues.append(clue1)
+        }
+        
+        if let clue2 = clue2TextField.text,
+            !clue2.isEmpty {
+            poi.clues.append(clue2)
+        }
+        
+        if let clue3 = clue3TextField.text,
+            !clue3.isEmpty {
+            poi.clues.append(clue3)
+        }
+        
+        delegate?.poiWasAdded(poi)
+    }
     
 }
