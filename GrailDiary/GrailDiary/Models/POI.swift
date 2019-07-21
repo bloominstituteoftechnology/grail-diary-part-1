fff0ac6115b612ea34cb1d5d2a2c7bb90a71965c
Percy Ngan <percival.ngan@gmail.com>
2019-07-20 20:00:07 -0500

    Working app

---
+++ GrailDiary/GrailDiary/Models/POI.swift
@@ -0,0 +1,15 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Percy Ngan on 7/19/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import Foundation
+
+struct POI {
+    let location: String
+    let country: String
+    var clues: [String]
+}
