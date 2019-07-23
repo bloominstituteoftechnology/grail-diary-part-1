e9672efd8aa9c534812d5d4c8d95fb3f6cda92e1
Vici Shaweddy <vshaweddy@gmail.com>
2019-07-23 14:29:36 -0400

    adding image

---
+++ GrailDiary/GrailDiary/CluesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Vici Shaweddy on 7/22/19.
+//  Copyright © 2019 Vici Shaweddy. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
