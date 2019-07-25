55a3970cb35f86749c2938b85b31deebb4517256
KALISA FISHER <kiecreations@gmail.com>
2019-07-25 13:07:39 -0400

    Create New Grail Diary Project - Part 1 Auto Layout

---
+++ GrailDiaryPart1-AutoLayout/GrailDiaryPart1-AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiaryPart1-AutoLayout
+//
+//  Created by Lisa Fisher on 7/25/19.
+//  Copyright © 2019 Lisa Fisher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
