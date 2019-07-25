a6d1416b772c0abfa3f62f25561f49d091141755
JosephRogers94 <Joerogersss2016@gmail.com>
2019-07-25 18:52:51 -0500

    Add Base .xcodeproj

---
+++ Grail Diary/Grail Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grail Diary
+//
+//  Created by Joseph Rogers on 7/25/19.
+//  Copyright © 2019 Joe Rogers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
