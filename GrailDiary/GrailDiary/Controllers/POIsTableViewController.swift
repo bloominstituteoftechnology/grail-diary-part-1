0363c9d5fedda30e3668963cd6d6b59034ef7a8b
John Kouris <jdkouris@gmail.com>
2019-07-24 20:41:34 -0700

    Created the POI model and added the UITableViewDataSource protocol to the POIsTableViewController

--- GrailDiary/GrailDiary/Controllers/POIsTableViewController.swift
+++ GrailDiary/GrailDiary/Controllers/POIsTableViewController.swift
@@ -9,22 +9,37 @@
 import UIKit
 
 class POIsTableViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var pointsOfInterest = [POI]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
     
 
-    /*
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
+    
 
+}
+
+extension POIsTableViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "POICell", for: indexPath)
+        return cell
+    }
+    
 }
