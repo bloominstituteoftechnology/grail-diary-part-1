ae57f1b518d69d6ed2f9204a7e0a03a82854acfd
John Kouris <jdkouris@gmail.com>
2019-07-24 20:32:59 -0700

    Added the necessary code files and set up MVC file structure

---
+++ GrailDiary/GrailDiary/Model/POI.swift
@@ -0,0 +1,9 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by John Kouris on 7/24/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import Foundation
---
+++ GrailDiary/GrailDiary/View/POITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  POITableViewCell.swift
+//  GrailDiary
+//
+//  Created by John Kouris on 7/24/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
