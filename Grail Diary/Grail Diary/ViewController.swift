ffd5a99fbb5674debb1abea6c9096728fd25c00f
Jessie Griffin <i.jessie.me@icloud.com>
2019-07-25 23:36:25 -0400

    Initial commit

---
+++ Grail Diary/Grail Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grail Diary
+//
+//  Created by Jessie Ann Griffin on 7/25/19.
+//  Copyright © 2019 Jessie Griffin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
