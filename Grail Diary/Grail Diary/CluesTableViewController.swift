ac8ab6bfbb2ab515be90745a7346cf7fb64241c4
Eoin Lavery <dev@tenantc.co.uk>
2019-07-26 04:45:46 +0100

    Initial Commit: - Completed AutoLayout tasks for Part 1

---
+++ Grail Diary/Grail Diary/CluesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grail Diary
+//
+//  Created by Eoin Lavery on 26/07/2019.
+//  Copyright © 2019 Eoin Lavery. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
