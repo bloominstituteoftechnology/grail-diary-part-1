f7af96861f2741762c2b4d66b54acad644766081
Clayton Bonaguidi <claytonbonaguidi@gmail.com>
2019-07-27 00:16:20 -0700

    MVP part 1

---
+++ Grail Diary/Grail Diary/CluesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grail Diary
+//
+//  Created by Clayton Bonaguidi on 7/26/19.
+//  Copyright © 2019 Clayton Bonaguidi. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
