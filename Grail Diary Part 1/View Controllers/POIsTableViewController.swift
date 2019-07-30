5124cddcdfe9ba07d134c00b1c20d11f5143b89c
dillonp23 <dillonp.lambda@gmail.com>
2019-07-30 15:47:10 -0700

    Added models, view controllers, and custom views groups. Added basic functionality to POITableViewController, and AddPOIViewController. Added POI struct to POI.swift.

---
+++ Grail Diary Part 1/View Controllers/POIsTableViewController.swift
@@ -0,0 +1,53 @@
+//
+//  POIsTableViewController.swift
+//  Grail Diary Part 1
+//
+//  Created by Dillon P on 7/29/19.
+//  Copyright © 2019 Dillon P. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    // MARK: - Outlets
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Properties
+    
+    var pois: [POI] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension POIsTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pois.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "POICell", for: indexPath) as? POITableViewCell else { return UITableViewCell() }
+        
+        let poi = pois[indexPath.row]
+        return cell
+    }
+    
+    
+}
