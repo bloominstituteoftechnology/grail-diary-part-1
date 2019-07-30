989c5ae5d3bd36f6d76f649d3512ad3d57f3557f
JosephRogers94 <Joerogersss2016@gmail.com>
2019-07-30 01:34:18 -0500

    AddPOIViewController Finished

--- Grail Diary/Grail Diary/View/AddPOIViewController.swift
+++ Grail Diary/Grail Diary/View/AddPOIViewController.swift
@@ -8,23 +8,74 @@
 
 import UIKit
 
+protocol AddPOIDelegate {
+    func poiWasAdded(_ poi: POI)
+}
+
 class AddPOIViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var clue1TextField: UITextField!
+    @IBOutlet weak var clue2TextField: UITextField!
+    @IBOutlet weak var clue3TextField: UITextField!
+    
+    var delegate: AddPOIDelegate?
+    // this override allows you to drag the keyboard down.
+    override func touchesBegan(_ touches: Set<UITouch>,
+                               with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationTextField.delegate = self
+        // Do any additional setup after loading the view.
     }
-    */
+    @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+        guard let location = locationTextField.text, let country = countryTextField.text else {return}
+        var newPOI = POI(location: location, country: country, clues: [])
+        if let clue1 = clue1TextField.text, let clue2 = clue2TextField.text, let clue3 = clue3TextField.text {
+            newPOI.clues.append(clue1)
+            newPOI.clues.append(clue2)
+            newPOI.clues.append(clue3)
+            delegate?.poiWasAdded(newPOI)
+        }
+    }
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
 
+
+extension AddPOIViewController: UITextFieldDelegate {
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let text = textField.text,
+            !text.isEmpty {
+            switch textField{
+            case locationTextField:
+                countryTextField.becomeFirstResponder()
+            case countryTextField:
+                clue1TextField.becomeFirstResponder()
+            case clue1TextField:
+                clue2TextField.becomeFirstResponder()
+            case clue2TextField:
+                clue3TextField.becomeFirstResponder()
+            default:
+                resignFirstResponder()
+            }
+        }
+        return false
+    }
 }
--- Grail Diary/Grail Diary/View/POIsTableViewController.swift
+++ Grail Diary/Grail Diary/View/POIsTableViewController.swift
@@ -8,15 +8,29 @@
 
 import UIKit
 
-class POIsTableViewController: UIViewController {
+var POIList: [POI] = []
 
+class POIsTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+    
+
+    @IBOutlet weak var TableViewOutlet: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        TableViewOutlet.delegate = self
         // Do any additional setup after loading the view.
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as? POITableViewCell else {fatalError("Something failed")}
+        
+        return cell
+    }
     /*
     // MARK: - Navigation
 
