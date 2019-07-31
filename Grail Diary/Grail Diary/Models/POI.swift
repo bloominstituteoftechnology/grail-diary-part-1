3b9c73c3f17111a21c44eab5dfba9559125f9f06
Jordan Skenandore <jordan.skenandore@gmail.com>
2019-07-30 22:35:16 -0600

    App is complete with the exception that locations are not showing on the main view controller

---
+++ Grail Diary/Grail Diary/Models/POI.swift
@@ -0,0 +1,15 @@
+//
+//  POI.swift
+//  Grail Diary
+//
+//  Created by Lambda_School_Loaner_141 on 7/30/19.
+//  Copyright © 2019 Jordan Skenandore. All rights reserved.
+//
+
+import Foundation
+
+struct POI {
+    let location: String
+    let country: String
+    var clues: [String]
+}
