605fea9a7da50fba88ef25c488c2330723b312a7
Christopher Gooley <chriswgooley@gmail.com>
2019-07-30 23:51:48 -0400

    Completed File Tasks, Storyboard tasks, and worked on POI.swift and two View Controllers. Up to step 17.

---
+++ Grail Diary/Grail Diary/Views/POITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  POITableViewCell.swift
+//  Grail Diary
+//
+//  Created by Chris Gooley on 7/30/19.
+//  Copyright © 2019 Chris Gooley. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
