890a934b135f445225d4c75dc27d6cc1a52e0c47
Clayton Bonaguidi <claytonbonaguidi@gmail.com>
2019-07-30 19:19:45 -0700

    Grail part 2

---
+++ Grail Diary/Grail Diary/Models/POI.swift
@@ -0,0 +1,15 @@
+//
+//  POI.swift
+//  Grail Diary
+//
+//  Created by Clayton Bonaguidi on 7/30/19.
+//  Copyright © 2019 Clayton Bonaguidi. All rights reserved.
+//
+
+import Foundation
+
+struct POI {
+    let location: String
+    let country: String
+    let clues: [String]
+}
