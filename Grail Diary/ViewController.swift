84adbb3a9c543d6e8f4e839dd66d63454db5a4a7
William Chen <chen.will7@gmail.com>
2019-08-13 12:39:04 -0700

    Initial Commit

---
+++ Grail Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grail Diary
+//
+//  Created by William Chen on 8/13/19.
+//  Copyright © 2019 William Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
