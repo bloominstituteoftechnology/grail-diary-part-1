b352bc00b06a64772ca018b76090217d9280d38a
ladybeitel <ciarabeitel@gmail.com>
2019-08-13 17:10:14 -0400

    Complete Stroyboard Tasks 1-12

---
+++ GrailDiary/View Controllers/CluesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CluesTableViewController.swift
+//  GrailDiary
+//
+//  Created by Ciara Beitel on 8/13/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
