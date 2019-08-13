7a81d5d1523c9e5aedd94c2f4edd5564be1a37d9
RoastBeefKazenakis <sabinowitz@gmail.com>
2019-08-13 15:03:32 -0400

    first commit

---
+++ GrailDiaryLayout/GrailDiaryLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiaryLayout
+//
+//  Created by Thomas Sabino-Benowitz on 8/13/19.
+//  Copyright © 2019 Thomas Sabino-Benowitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
