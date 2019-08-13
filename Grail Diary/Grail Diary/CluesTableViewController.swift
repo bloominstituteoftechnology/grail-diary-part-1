e38109f51ed1fe86e9677d95c1bdb7c0732b716e
ryandutson <mr.dutson@gmail.com>
2019-08-13 16:47:05 -0600

    Project completed according to directions.

---
+++ Grail Diary/Grail Diary/CluesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CluesTableViewController.swift
+//  Grail Diary
+//
+//  Created by Ryan Dutson on 8/13/19.
+//  Copyright © 2019 Ryan Dutson. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
