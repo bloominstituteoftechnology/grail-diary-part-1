b60f5d0969ec9af0febe4deac8a2e6860c0a4413
bfvilchez <bfvilchez@icloud.com>
2019-08-13 16:57:27 -0600

    finished stroyBoard

---
+++ grail diary part 1/grail diary part 1/CluesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  grail diary part 1
+//
+//  Created by brian vilchez on 8/13/19.
+//  Copyright © 2019 brian vilchez. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
