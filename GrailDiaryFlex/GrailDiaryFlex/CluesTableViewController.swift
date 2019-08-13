0030bdc515713b90a255d5f5427e206b8de87579
JerryHaaser <jerry.haaser@hotmail.com>
2019-08-13 14:45:05 -0500

    Changed dame of view controller to CluesTableViewCOntroller, added tableview, embedded in navigation contoller

---
+++ GrailDiaryFlex/GrailDiaryFlex/CluesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CluesTableViewController.swift
+//  GrailDiaryFlex
+//
+//  Created by admin on 8/13/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
