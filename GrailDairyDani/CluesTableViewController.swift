fe152e9c10182300994bd570046d136de376fd89
DaniBlackwell <bleuxgenius@gmail.com>
2019-08-13 17:09:12 -0500

    GrailStoryBoard

---
+++ GrailDairyDani/CluesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDairyDani
+//
+//  Created by Danielle Blackwell on 8/13/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
