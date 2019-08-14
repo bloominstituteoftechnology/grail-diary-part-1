a68125d658e797a9cebb6612661875ef757d0344
bfvilchez <bfvilchez@icloud.com>
2019-08-14 14:02:31 -0600

    set up tableview controller

--- grail diary part 1/grail diary part 1/controller/CluesTableViewController.swift
+++ grail diary part 1/grail diary part 1/controller/CluesTableViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class CluesTableViewController: UIViewController {
 
+    @IBOutlet weak var tableview: UITableView!
+    var POIS: [POI] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      tableview.dataSource = self
     }
 
 
 }
 
+extension CluesTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return POIS.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "POICell", for: indexPath) as? POITableViewCell else {return UITableViewCell()}
+        let POI = POIS[indexPath.row]
+        cell.poi = poi
+        return cell
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "" {
+            guard let selectedPath = tableview.indexPathForSelectedRow,
+                let AddPOiVC = segue.destination as? AddPOIViewController else {return}
+            
+        } else if segue.identifier == "" {
+            guard let POiDetailVC = segue.destination as? POIDetailViewController else {return}
+        }
+    }
+    
+}
+extension CluesTableViewController: CreatePOI {
+    func createPOI(poi: POI) {
+        POIS.append(poi)
+        dismiss(animated: true, completion: nil)
+        tableview.reloadData()
+    }
+    
+    
+}
