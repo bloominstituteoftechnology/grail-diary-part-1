d493a39511e009430920e63c533a5c282b8ceef0
ladybeitel <ciarabeitel@gmail.com>
2019-08-14 18:46:12 -0400

    Completed tasks in POIsTableViewController.swift and AddPOIViewController.swift (steps 7-20) Stuck on step 21

--- GrailDiary/View Controllers/POIsTableViewController.swift
+++ GrailDiary/View Controllers/POIsTableViewController.swift
@@ -10,21 +10,34 @@ import UIKit
 
 class POIsTableViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var pois: [POI] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension POIsTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pois.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath) as? POITableViewCell else { return UITableViewCell() }
+        
+        let poi = pois[indexPath.row]
+        cell.poi = poi
+        
+        return cell
+    }
+    
+    
 }
