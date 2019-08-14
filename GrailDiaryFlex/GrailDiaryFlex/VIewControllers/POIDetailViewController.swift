afb00fc09c7c8d7576af4666bdd680eb9e63f366
JerryHaaser <jerry.haaser@hotmail.com>
2019-08-14 16:08:55 -0500

    added outlets in POIDetailViewController and set corresponding text

--- GrailDiaryFlex/GrailDiaryFlex/VIewControllers/POIDetailViewController.swift
+++ GrailDiaryFlex/GrailDiaryFlex/VIewControllers/POIDetailViewController.swift
@@ -9,9 +9,17 @@
 import UIKit
 
 class POIDetailViewController: UIViewController {
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var cluesTextView: UITextView!
+    
+    var poi: POI?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
 
         // Do any additional setup after loading the view.
     }
@@ -26,5 +34,17 @@ class POIDetailViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    private func updateViews() {
+        guard let poi = poi else { return }
+        
+        locationLabel.text = poi.location
+        countryLabel.text = poi.country
+        var cluesText = ""
+        for clue in poi.clues {
+            cluesText += "\(clue)"
+        }
+        
+    }
 
 }
