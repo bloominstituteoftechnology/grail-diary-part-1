22c7804e3b2c4108c6f51adb1beaffde501914b0
JerryHaaser <jerry.haaser@hotmail.com>
2019-08-14 14:11:58 -0500

    Added POIs AddPOI POIDetail view controller files, add POITableViewCell file, and POI model file

---
+++ GrailDiaryFlex/GrailDiaryFlex/POI.swift
@@ -0,0 +1,9 @@
+//
+//  POI.swift
+//  GrailDiaryFlex
+//
+//  Created by admin on 8/14/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import Foundation
---
+++ GrailDiaryFlex/GrailDiaryFlex/VIewControllers/POIDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  POIDetailViewController.swift
+//  GrailDiaryFlex
+//
+//  Created by admin on 8/14/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class POIDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
