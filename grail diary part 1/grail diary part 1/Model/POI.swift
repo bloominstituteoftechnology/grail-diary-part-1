0e5c29a0a2025266d500c751622d9b00cee12938
bfvilchez <bfvilchez@icloud.com>
2019-08-14 13:35:25 -0600

    organized the file navigator plus created cocoa touch classes

---
+++ grail diary part 1/grail diary part 1/Model/POI.swift
@@ -0,0 +1,12 @@
+//
+//  POI.swift
+//  grail diary part 1
+//
+//  Created by brian vilchez on 8/14/19.
+//  Copyright © 2019 brian vilchez. All rights reserved.
+//
+
+import Foundation
+struct POI {
+    
+}
---
+++ grail diary part 1/grail diary part 1/controller/AddPOIViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddPOIViewController.swift
+//  grail diary part 1
+//
+//  Created by brian vilchez on 8/14/19.
+//  Copyright © 2019 brian vilchez. All rights reserved.
+//
+
+import UIKit
+
+class AddPOIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
