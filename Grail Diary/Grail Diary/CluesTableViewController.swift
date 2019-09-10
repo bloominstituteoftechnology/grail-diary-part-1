63c80fe73fa615f49634ca5cf5eda6020431381f
Wilma <kima_92@fastmail.com>
2019-09-10 17:42:52 -0400

    Completed part 1 of Grail Diary Project

---
+++ Grail Diary/Grail Diary/CluesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CluesTableViewController.swift
+//  Grail Diary
+//
+//  Created by macbook on 9/10/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
