c267407c331f097374d46e42189f6a656fa763e1
Dennis <dpr1996@gmail.com>
2019-09-10 16:22:04 -0400

    Complete Project

---
+++ GrailDiary/GrailDiary/CluesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CluesTableViewController.swift
+//  GrailDiary
+//
+//  Created by Dennis on 9/10/19.
+//  Copyright © 2019 LambdaSchool. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
