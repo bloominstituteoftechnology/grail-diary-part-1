e012951b043a233d07e967e0a5f840d08a88abf2
Gi Pyo Kim <gipyok@gmail.com>
2019-09-10 12:58:49 -0700

    Completed UI Set up

---
+++ GrailDiary/GrailDiary/CluesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CluesTableViewController.swift
+//  GrailDiary
+//
+//  Created by Gi Pyo Kim on 9/10/19.
+//  Copyright © 2019 GIPGIP Studio. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
