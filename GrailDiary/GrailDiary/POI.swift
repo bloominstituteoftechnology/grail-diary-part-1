7a3a0f77da8f51d8c5c9f64f4facdc23b2172021
Jonalynn Masters <jonalynna.masters@gmail.com>
2019-09-12 15:45:34 -0700

    second commit: standup report

---
+++ GrailDiary/GrailDiary/POI.swift
@@ -0,0 +1,16 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Jonalynn Masters on 9/12/19.
+//  Copyright © 2019 Jonalynn Masters. All rights reserved.
+//
+
+import Foundation
+
+struct POI {
+    let location: String
+    let country: String
+    var clues: [String] = []
+    
+}
---
+++ GrailDiary/GrailDiary/POITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  POITableViewCell.swift
+//  GrailDiary
+//
+//  Created by Jonalynn Masters on 9/12/19.
+//  Copyright © 2019 Jonalynn Masters. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
