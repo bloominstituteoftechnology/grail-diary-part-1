af6d66b67b827e5da06a560fd1d3f59a47362df6
Jesse Ruiz <jesseleeruiz@Jesses-Air.home>
2019-09-12 13:22:21 -1000

    Finished Project

--- GrailDiary/GrailDiary/AddPOIViewController.swift
+++ GrailDiary/GrailDiary/AddPOIViewController.swift
@@ -74,5 +74,6 @@ extension AddPOIViewController: UITextFieldDelegate {
                 textField.resignFirstResponder()
             }
         }
+        return false
     }
 }
--- GrailDiary/GrailDiary/POIsTableViewController.swift
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -22,9 +22,13 @@ class POIsTableViewController: UIViewController {
             if let addPOIVC = segue.destination as? AddPOIViewController {
                 addPOIVC.delegate = self
             }
+        } else if segue.identifier == "ShowPOIDetailSegue" {
+            if let indexPath = tableView.indexPathForSelectedRow,
+                let poiDetailVC = segue.destination as? POIDetailViewController {
+                poiDetailVC.poi = POIModelsArray[indexPath.row]
+            }
         }
     }
-
 }
 
 extension POIsTableViewController: UITableViewDataSource {
@@ -33,17 +37,20 @@ extension POIsTableViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "POICell", for: indexPath)
+            as? POITableViewCell else { return UITableViewCell() }
+        
+        let poi = POIModelsArray[indexPath.row]
+        cell.poi = poi
+        
+        return cell
     }
-    
-    
 }
 
 extension POIsTableViewController: AddPOIDelegate {
     func poiWasAdded(_ poi: POI) {
-        poi.append(POIModelsArray)
+        POIModelsArray.append(poi)
         dismiss(animated: true, completion: nil)
         tableView.reloadData()
     }
-    
 }
