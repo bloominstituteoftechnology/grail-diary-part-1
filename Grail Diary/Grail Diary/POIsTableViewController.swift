9dcb6e9ebe5669c55950d8adb27583372fd80613
Wilma <kima_92@fastmail.com>
2019-09-12 19:57:02 -0400

    Completed steps 1 - 33 of Challenge

--- Grail Diary/Grail Diary/POIsTableViewController.swift
+++ Grail Diary/Grail Diary/POIsTableViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class POIsTableViewController: UIViewController {
     
-    var poi: [POI] = []
+    var poiArray: [POI] = []
+    
+    @IBOutlet weak var tableView: UITableView!
     
 
     override func viewDidLoad() {
@@ -20,14 +22,62 @@ class POIsTableViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "AddPOIModalSegue" {
+            if let addPoiVC = segue.destination as? AddPOIViewController {
+                addPoiVC.delegate = self
+            }
+            
+        } 
+        
+        
+//        if segue.identifier == "AddFriendModalSegue" {
+//            if let addFriendVC = segue.destination as? AddFriendViewController {
+//                addFriendVC.delegate = self
+//            }
+//        } else if segue.identifier == "ShowFriendDetailSegue" {
+//            if let indexPath = tableView.indexPathForSelectedRow,
+//                let friendDetailVC = segue.destination as? FriendDetailViewController {
+//                friendDetailVC.friend = friends[indexPath.row]
+//            }
+//
+        
+        
+        
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+ 
 
 }
+
+extension POIsTableViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
+
+extension POIsTableViewController: AddPOIDelegate {
+    func poiWasAdded(_ poi: POI) {
+        poiArray.append(poi)
+        dismiss(animated: true, completion: nil)
+        tableView.reloadData()
+    }
+    
+    
+}
+
+
+//Wire up the delegate property of the tableview in the storyboard to the view controller
