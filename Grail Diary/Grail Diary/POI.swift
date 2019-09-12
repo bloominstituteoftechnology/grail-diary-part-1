e50cb0136f38facabc817cd6d7b78aa3808f8da1
Wilma <kima_92@fastmail.com>
2019-09-12 15:56:06 -0400

    Created new files

---
+++ Grail Diary/Grail Diary/POI.swift
@@ -0,0 +1,16 @@
+//
+//  POI.swift
+//  Grail Diary
+//
+//  Created by macbook on 9/12/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct  POI {
+    
+    var location: String
+    var country: String
+    var clues: [String]
+}
