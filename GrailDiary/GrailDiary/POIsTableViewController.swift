05e273b26073ae4ff805fbf2344b2a3170b85983
Craig Swanson <cswanson02@me.com>
2019-09-19 23:15:02 -0500

    Added view controller with preliminary table view and cells.

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  POIsTableViewController.swift
+//  GrailDiary
+//
+//  Created by Craig Swanson on 9/19/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
