9adb3a041b1c5d0883f0c2f9f5a95ef9cd26f720
Denis Cedeno <denis.cedeno@gmail.com>
2019-09-21 12:39:11 -0400

    renamed View controller to POIsTableViewController, Embeded the single View Controller scene in a Navigation Controller,  Dragged in a Table View to that View Controller, Set the navigation bar to show large titles, Gave the View Controller scene a title of "Grail Diary", added constraints to the table view, set prototype cell to 1 in table view, set identifier to "POICell", set table view to show 60pt rows

---
+++ GrailDairy/GrailDairy/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDairy
+//
+//  Created by denis cedeno on 9/19/19.
+//  Copyright © 2019 DenCedeno Co. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
