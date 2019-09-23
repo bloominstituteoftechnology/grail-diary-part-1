46e6053ef9782d866275ebb57e582897abc98528
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-09-22 21:47:56 -0400

    Created project and set up List of Views view.

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  POIsTableViewController.swift
+//  GrailDiary
+//
+//  Created by Christy Hicks on 9/22/19.
+//  Copyright © 2019 Knight Night. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
