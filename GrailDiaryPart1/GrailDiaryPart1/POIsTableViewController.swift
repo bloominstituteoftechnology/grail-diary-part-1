4f8e451fa591756ee31c121917903159eb0c6e4e
Christopher Price <christopher.r.price@gmail.com>
2019-09-24 22:10:08 -0400

    Set up the prototype cell and added constraints to that. Dragged out the 2nd and 3rd VCs, but didn't set them up.

---
+++ GrailDiaryPart1/GrailDiaryPart1/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiaryPart1
+//
+//  Created by Chris Price on 9/20/19.
+//  Copyright © 2019 Chris Price. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
