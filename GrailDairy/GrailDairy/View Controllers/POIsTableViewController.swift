32fcefab3acbd7c24c986bf2f7e297f9f80e80b4
Denis Cedeno <denis.cedeno@gmail.com>
2019-09-26 16:45:30 -0400

    connected view controller to table view

--- GrailDairy/GrailDairy/View Controllers/POIsTableViewController.swift
+++ GrailDairy/GrailDairy/View Controllers/POIsTableViewController.swift
@@ -1,8 +1,8 @@
 //
-//  ViewController.swift
+//  POIsTableViewController.swift
 //  GrailDairy
 //
-//  Created by denis cedeno on 9/19/19.
+//  Created by denis cedeno on 9/26/19.
 //  Copyright © 2019 DenCedeno Co. All rights reserved.
 //
 
@@ -10,11 +10,26 @@ import UIKit
 
 class POIsTableViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var poi: [POI] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
