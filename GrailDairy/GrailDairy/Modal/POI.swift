f4e1f76ae1919b1d6176b17792f1864b0256d6b8
Denis Cedeno <denis.cedeno@gmail.com>
2019-09-26 16:27:13 -0400

    Reorganized file structure adding custom views, modal and view controllers

---
+++ GrailDairy/GrailDairy/Modal/POI.swift
@@ -0,0 +1,9 @@
+//
+//  POI.swift
+//  GrailDairy
+//
+//  Created by denis cedeno on 9/26/19.
+//  Copyright © 2019 DenCedeno Co. All rights reserved.
+//
+
+import Foundation
---
+++ GrailDairy/GrailDairy/View Controllers/POIDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  POIDetailViewController.swift
+//  GrailDairy
+//
+//  Created by denis cedeno on 9/26/19.
+//  Copyright © 2019 DenCedeno Co. All rights reserved.
+//
+
+import UIKit
+
+class POIDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
