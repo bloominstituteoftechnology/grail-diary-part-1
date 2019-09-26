a31f7e50a55bfb1d08bcda59c8ebd0973f676084
Denis Cedeno <denis.cedeno@gmail.com>
2019-09-26 17:02:57 -0400

    implemented protocols and constructed delagate for tableview as an extension of table view controller

--- GrailDairy/GrailDairy/View Controllers/POIsTableViewController.swift
+++ GrailDairy/GrailDairy/View Controllers/POIsTableViewController.swift
@@ -13,23 +13,30 @@ class POIsTableViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    var poi: [POI] = []
+    var pois: [POI] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+
+}
+
+extension POIsTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pois.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "POIcell", for: indexPath) as? POITableViewCell else {
+            fatalError()
+        }
+        
+        let poi = pois[indexPath.row]
+        cell.POI = poi
+        
+        return cell
     }
-    */
-
+    
+    
 }
