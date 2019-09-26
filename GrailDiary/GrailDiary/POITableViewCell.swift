7f64282e84a77b9827c1d6430f809f3ef6013421
Craig Swanson <cswanson02@me.com>
2019-09-26 00:04:06 -0500

    Through step 11 of instructions

---
+++ GrailDiary/GrailDiary/POITableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  POITableViewCell.swift
+//  GrailDiary
+//
+//  Created by Craig Swanson on 9/25/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+    
+    var poi: POI? {
+        didSet {
+            updateViews()
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- GrailDiary/GrailDiary/POIsTableViewController.swift
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
+
 class POIsTableViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var pois: [POI] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,5 +20,23 @@ class POIsTableViewController: UIViewController {
     }
 
 
+}
+
+extension POIsTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        pois.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "POITableViewCell", for: indexPath) as?
+            POITableViewCell else { return UITableViewCell() }
+        
+        let poi = pois[indexPath.row]
+        cell.poi = poi
+        
+        return cell
+    }
+    
+    
 }
 
