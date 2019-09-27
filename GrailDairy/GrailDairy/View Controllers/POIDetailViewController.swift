7f1258a68cbd76c04a958e081414f5b6165764e2
Denis Cedeno <denis.cedeno@gmail.com>
2019-09-26 21:17:09 -0400

    configures POIDetailViewController, created label and text view outlets, created poi variable and configured updateViews

--- GrailDairy/GrailDairy/View Controllers/POIDetailViewController.swift
+++ GrailDairy/GrailDairy/View Controllers/POIDetailViewController.swift
@@ -9,22 +9,28 @@
 import UIKit
 
 class POIDetailViewController: UIViewController {
-
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var cluesTextView: UITextView!
+    
+    var poi: POI?
+    
+    private func updateViews() {
+        guard let poi = poi, isViewLoaded else { return }
+        locationLabel.text = poi.location
+        countryLabel.text = poi.country
+        
+        var cluesText = ""
+        for clue in poi.clues {
+            cluesText += "• \(clue)\n"
+        }
+        cluesTextView.text = cluesText
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateViews()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
