e8d29cedbfdc9ebd9a8acf14a8895d085c8cdad8
Dennis Rudolph <Dennis@Denniss-MacBook-Pro.local>
2019-10-08 15:52:32 -0400

    iOS11 Part 1 MVP

---
+++ GrailDiary/GrailDiary/CluesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CluesTableViewController.swift
+//  GrailDiary
+//
+//  Created by Dennis Rudolph on 10/8/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
