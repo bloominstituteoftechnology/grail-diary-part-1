a78eef3515960d5ee409c037076c8de3c7998288
Pioneer Pass <pioneerpass@protonmail.com>
2019-10-08 12:09:23 -0700

    FINISHED

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Thomas Bernard Leonard II on 10/8/19.
+//  Copyright © 2019 Thomas Bernard Leonard II. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
