d3b3c16e37fe80876a68b165f42189f45ffee6ee
Jon Bash <jonbash@protonmail.com>
2019-10-08 12:12:09 -0700

    init xcode project

---
+++ GrailDiary/GrailDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Jon Bash on 2019-10-08.
+//  Copyright © 2019 Jon Bash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
