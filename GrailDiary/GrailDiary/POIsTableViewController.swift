4de43b18d2e836ea3fced549805e817d0f7799f8
Dan M <djmthedev@gmail.com>
2019-10-08 12:52:54 -0700

    MVP

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by morse on 10/8/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+    
+    enum PropertyKeys: String {
+        case addPOIModalSegue = "AddPOIModalSegue"
+        case showPOIDetailSegue = "ShowPOIDetailSegue"
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
