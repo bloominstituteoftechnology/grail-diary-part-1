02ddfe84002afc15079f2ec536234adc8e8d76f6
Jon Bash <jonbash@protonmail.com>
2019-10-10 12:51:16 -0700

    implement -> step 10

--- GrailDiary/GrailDiary/View Controllers/POIsTableViewController.swift
+++ GrailDiary/GrailDiary/View Controllers/POIsTableViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class POIsTableViewController: UIViewController {
+    
+    var pois: [POI] = []
+    
+    @IBOutlet weak var poiTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,3 +22,19 @@ class POIsTableViewController: UIViewController {
 
 }
 
+extension POIsTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pois.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell =
+            tableView.dequeueReusableCell(withIdentifier: "FriendCell", for: indexPath)
+                as? POITableViewCell
+            else { return UITableViewCell() }
+        
+        cell.poi = pois[indexPath.row]
+        
+        return cell
+    }
+}
