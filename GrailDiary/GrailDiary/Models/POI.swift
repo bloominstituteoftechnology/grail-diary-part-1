6b99ab41eabc496c68d0f7ee6d60dd97b0610049
Jon Bash <jonbash@protonmail.com>
2019-10-10 12:39:33 -0700

    set up storyboard for part 2

---
+++ GrailDiary/GrailDiary/Models/POI.swift
@@ -0,0 +1,9 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Jon Bash on 2019-10-10.
+//  Copyright © 2019 Jon Bash. All rights reserved.
+//
+
+import Foundation
---
+++ GrailDiary/GrailDiary/Views/POITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  POITableViewCell.swift
+//  GrailDiary
+//
+//  Created by Jon Bash on 2019-10-10.
+//  Copyright © 2019 Jon Bash. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
