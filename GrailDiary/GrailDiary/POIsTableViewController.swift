6c1404db389249776e91a69d1f3c3f42879a99a1
Dan M <djmthedev@gmail.com>
2019-10-09 19:47:11 -0700

    set POITableViewController as POITableView's datasource.

--- GrailDiary/GrailDiary/POIsTableViewController.swift
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -8,17 +8,34 @@
 
 import UIKit
 
-class POIsTableViewController: UIViewController {
+class POIsTableViewController: UIViewController, UITableViewDataSource {
     
-    enum PropertyKeys: String {
-        case addPOIModalSegue = "AddPOIModalSegue"
-        case showPOIDetailSegue = "ShowPOIDetailSegue"
+    @IBOutlet var POITableView: UITableView!
+    
+    struct PropertyKeys {
+        static let addPOIModalSegue = "AddPOIModalSegue"
+        static let showPOIDetailSegue = "ShowPOIDetailSegue"
+        static let POICell = "POICell"
     }
+    
+    var POIs: [POI] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: - TableViewDataSource
+    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return POIs.count
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: PropertyKeys.POICell, for: indexPath) as? POITableViewCell else { fatalError("no cell") }
+//        
+//        return cell
+//    }
 
 
 }
