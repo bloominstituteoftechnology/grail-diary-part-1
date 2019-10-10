459787d1e6c09202659b567877ef4b3c012d8137
Brandi <blubrandi0@gmail.com>
2019-10-10 13:43:28 -0600

    Added POI model

---
+++ GrailDiary/GrailDiary/Models/POI.swift
@@ -0,0 +1,15 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Brandi on 10/10/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct POT {
+    let location: String
+    let country: String
+    var clues: [String]
+}
