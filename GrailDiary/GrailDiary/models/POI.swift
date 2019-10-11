1179d88e7265352e7a23fa6c701f0354c5fe6644
Thomas Sabino-Benowitz <sabinowitz@gmail.com>
2019-10-10 20:48:59 -0400

    wiring, non-storyboard elements

---
+++ GrailDiary/GrailDiary/models/POI.swift
@@ -0,0 +1,9 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Thomas Sabino-Benowitz on 10/10/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ GrailDiary/GrailDiary/views/POITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  POITableViewCell.swift
+//  GrailDiary
+//
+//  Created by Thomas Sabino-Benowitz on 10/10/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
