0159c09e70c62b0fb1c9dfe395359001aa082894
Chad Rutherford <chad_rutherford@outlook.com>
2019-11-05 14:38:01 -0500

    Created Xcode Project

---
+++ GrailDiary/GrailDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Chad Rutherford on 11/5/19.
+//  Copyright © 2019 Chad Rutherford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
