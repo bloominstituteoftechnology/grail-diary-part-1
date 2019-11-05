168e1d62c9285d98994970215b5d4c78f84ccc08
Chad Rutherford <chad_rutherford@outlook.com>
2019-11-05 14:47:34 -0500

    Finished 'POIsTableViewController'

--- GrailDiary/GrailDiary/POIsTableViewController.swift
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  POIsTableViewController.swift
 //  GrailDiary
 //
 //  Created by Chad Rutherford on 11/5/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class POIsTableViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
