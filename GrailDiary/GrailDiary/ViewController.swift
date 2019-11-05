7847e12a7e1bcba3ec442cb3720b98067c8665df
Tobi Tselios <tobi.tselios@gmail.com>
2019-11-05 21:11:57 +0000

    List of Clues View

---
+++ GrailDiary/GrailDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Tobi Kuyoro on 05/11/2019.
+//  Copyright © 2019 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
