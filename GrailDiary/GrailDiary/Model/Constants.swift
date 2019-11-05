64b8508d496cbc1ce2d786fa99fe314c26fead64
Chad Rutherford <chad_rutherford@outlook.com>
2019-11-05 14:57:02 -0500

    Organized Structure and Finished 'AddNewPOIViewController'

---
+++ GrailDiary/GrailDiary/Model/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  GrailDiary
+//
+//  Created by Chad Rutherford on 11/5/19.
+//  Copyright © 2019 Chad Rutherford. All rights reserved.
+//
+
+import Foundation
+
+
+struct Cells {
+    static let poiCell = "POICell"
+}
+
+struct Segues {
+    static let addPOIModalSegue = "AddPOIModalSegue"
+}
