c077791098f7ebb9f1f040cfa90d124677bcbf2f
donellabarcelo <donellabarcelo95@gmail.com>
2019-11-05 13:05:25 -0800

    Embed view controller in a navigation controller. Added table view to scene and set constraints.

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  POIsTableViewController.swift
+//  GrailDiary
+//
+//  Created by Donella Barcelo on 11/5/19.
+//  Copyright © 2019 Donella Barcelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
