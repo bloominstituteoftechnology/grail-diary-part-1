1ba6577749134119717e9b19269e74d14d022f18
Zack Larsen <zacklarsen1@gmail.com>
2019-11-05 19:58:18 -0700

    Final Product

---
+++ GrailDiary/GrailDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Zack Larsen on 11/5/19.
+//  Copyright © 2019 Zack Larsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
