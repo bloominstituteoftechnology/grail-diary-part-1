6a925ff1893035d920d207736f00370e0af766a8
Zack Larsen <zacklarsen1@gmail.com>
2019-11-07 13:28:20 -0700

    first iboutlet

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  POIsTableViewController.swift
+//  GrailDiary
+//
+//  Created by Zack Larsen on 11/7/19.
+//  Copyright © 2019 Zack Larsen. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    var POIs: [POI] = [
+    POI(location: "Cebu", country: "Philippines", clues: ["1","2","3"])
+    ]
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
