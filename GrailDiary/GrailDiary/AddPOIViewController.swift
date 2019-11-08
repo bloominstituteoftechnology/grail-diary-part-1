53dad85fb16837dd61ae4382de3b8f0b8ae01430
donellabarcelo <donellabarcelo95@gmail.com>
2019-11-07 18:54:22 -0800

    Wired up AddPOI view controller

--- GrailDiary/GrailDiary/AddPOIViewController.swift
+++ GrailDiary/GrailDiary/AddPOIViewController.swift
@@ -8,23 +8,67 @@
 
 import UIKit
 
-class AddPOIViewController: UIViewController {
+protocol AddPOIDelegate {
+    func poiWasAdded(_ poi: POI)
+}
 
+class AddPOIViewController: UIViewController {
+    
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var clue1TextField: UITextField!
+    @IBOutlet weak var clue2TextField: UITextField!
+    @IBOutlet weak var clue3TextField: UITextField!
+    
+    var delegate: AddPOIDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        locationTextField.delegate = self
+        countryTextField.delegate = self
+        clue1TextField.delegate = self
+        clue2TextField.delegate = self
+        clue3TextField.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
+    @IBAction func saveTapped(_ sender: Any) {
+        
+        if let location = locationTextField.text,
+            let country = countryTextField.text,
+            !location.isEmpty,
+            !country.isEmpty {
+            
+            var poi = POI(location: location, country: country, clues: [])
+            
+            if let clue1 = clue1TextField.text,
+                !clue1.isEmpty {
+                poi.clues.append(clue1)
+            }
+            
+            if let clue2 = clue2TextField.text,
+                !clue2.isEmpty {
+                poi.clues.append(clue2)
+            }
+            
+            if let clue3 = clue3TextField.text,
+                !clue3.isEmpty {
+                poi.clues.append(clue3)
+            }
+            
+            delegate?.poiWasAdded(poi)
+        }
+    }
+}
 
+extension AddPOIViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
 }
--- GrailDiary/GrailDiary/POIsTableViewController.swift
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -8,9 +8,17 @@
 
 import UIKit
 
-class POIsTableViewController: UIViewController, UITableViewDataSource {
-        
-    var pois: [POI] = []
+class POIsTableViewController: UIViewController, UITableViewDataSource, AddPOIDelegate {
+ 
+    var pois: [POI] = [
+        POI(location: "Hilo, Hawaii", country: "United States", clues: ["1", "2", "3"])
+    ]
+    
+    func poiWasAdded(_ poi: POI) {
+        pois.append(poi)
+        tableView.reloadData()
+        dismiss(animated: true, completion: nil)
+     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pois.count
@@ -23,20 +31,30 @@ class POIsTableViewController: UIViewController, UITableViewDataSource {
         
         let poi = pois[indexPath.row]
         
+        cell.poi = poi
         
         return cell
         
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "AddPOIModalSegue" {
+            if let addPOIVC = segue.destination as? AddPOIViewController {
+                addPOIVC.delegate = self
+            }
+                } else if segue.identifier == "ShowPOIDetailSegue" {
+                if let poiDetailVC = segue.destination as? POIDetailViewController,
+                    let indexPath = tableView.indexPathForSelectedRow {
+                    
+                    let poi = pois[indexPath.row]
+                    poiDetailVC.poi = poi
+            }
+        }
     }
-    */
-
 }
