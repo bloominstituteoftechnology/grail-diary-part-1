272eac180c4a910b5671d71fe39cd3a16342a7aa
donellabarcelo <donellabarcelo95@gmail.com>
2019-11-07 16:49:41 -0800

    Added IBOutlets to table view controller and conformed class to protocol

--- GrailDiary/GrailDiary/POIsTableViewController.swift
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -8,12 +8,23 @@
 
 import UIKit
 
-class POIsTableViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class POIsTableViewController: UIViewController, UITableViewDataSource {
+        
+    var pois: [POI] = []
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var numberOfCluesLabel: UILabel!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pois.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "POICell", for: indexPath)
+        
+        return cell
+        
     }
     
 
