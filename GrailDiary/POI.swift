879690f05ab68937c39868cedf5a925f5c749d92
Özgün Yildiz <ozgun.yildiz@hotmail.de>
2019-11-08 16:41:01 +0100

    finished

---
+++ GrailDiary/POI.swift
@@ -0,0 +1,9 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Sezen Bozdogan on 08.11.19.
+//  Copyright © 2019 Özgün Yildiz. All rights reserved.
+//
+
+import Foundation
---
+++ GrailDiary/POITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  POITableViewCell.swift
+//  GrailDiary
+//
+//  Created by Sezen Bozdogan on 08.11.19.
+//  Copyright © 2019 Özgün Yildiz. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
