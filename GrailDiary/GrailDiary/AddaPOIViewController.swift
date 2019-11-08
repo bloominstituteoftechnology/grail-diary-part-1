55f4d5a5af2286472174398c1e0c1e6068cbfc07
Zack Larsen <zacklarsen1@gmail.com>
2019-11-07 17:41:24 -0700

    if let statments did me dirty

--- GrailDiary/GrailDiary/AddaPOIViewController.swift
+++ GrailDiary/GrailDiary/AddaPOIViewController.swift
@@ -7,16 +7,66 @@
 //
 
 import UIKit
+protocol addPOIDelegate: class {
+func poiWasAdded(_poi: POI)
+    
+}
+
+
+
+
+
 
 class AddaPOIViewController: UIViewController {
-
+    
+    var delegate: addPOIDelegate?
+    
+    @IBOutlet weak var addLocationCell: UITextField!
+    @IBOutlet weak var addCountry: UITextField!
+    
+    @IBOutlet weak var addClue1: UITextField!
+    @IBOutlet weak var addClue2: UITextField!
+    
+    @IBOutlet weak var addClue3: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
+        }
+    @IBAction func cancelTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
+    @IBAction func saveTapped(_ sender: Any) {
+        guard let location = addLocationCell.text,
+            let country = addCountry.text  else  { return }
+    
+        var savedPoi = POI(location: location, country: country, clues: [])
+        
+        
+        if let clue1 = addClue1.text,
+        !clue1.isEmpty {
+            savedPoi.clues.append(clue1)
+            
+        }
+        if let clue2 = addClue2.text,
+        !clue2.isEmpty {
+            savedPoi.clues.append(clue2)
+            
+        }
+            if let clue3 = addClue3.text,
+                !clue3.isEmpty {
+             savedPoi.clues.append(clue3)
+                    
+        }
+    }
+}
+    
+    
     
 
+        // Do any additional setup after loading the vi
     /*
     // MARK: - Navigation
 
--- GrailDiary/GrailDiary/POIsTableViewController.swift
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -9,22 +9,37 @@
 import UIKit
 
 class POIsTableViewController: UIViewController {
-
-    var POIs: [POI] = [
-    POI(location: "Cebu", country: "Philippines", clues: ["1","2","3"])
-    ]
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var tableView: UITableView!
+    var placesOfInterest: [POI] = []
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
     
 
+    
+    
+   
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+extension POIsTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 }
