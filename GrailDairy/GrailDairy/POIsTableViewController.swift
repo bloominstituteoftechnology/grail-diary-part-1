a61d504a83fbca7d8102d61c33be2a8179254793
Jeremy Taylor <jtaylor@jctaylor.org>
2019-11-16 12:16:58 -0600

    Completed List of Clues View

---
+++ GrailDairy/GrailDairy/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  POIsTableViewController.swift
+//  GrailDairy
+//
+//  Created by Jeremy Taylor on 11/16/19.
+//  Copyright © 2019 Bytes-Random L.L.C. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
