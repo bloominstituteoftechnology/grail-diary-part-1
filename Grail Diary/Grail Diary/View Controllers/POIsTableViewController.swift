85b0828188adac9925f1675dcef824a5f0c6d60d
John Holowesko <john@sipsip.io>
2019-11-17 15:40:42 -0500

    Initial Commit

---
+++ Grail Diary/Grail Diary/View Controllers/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  POIsTableViewController.swift
+//  Grail Diary
+//
+//  Created by John Holowesko on 11/17/19.
+//  Copyright © 2019 John Holowesko. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
