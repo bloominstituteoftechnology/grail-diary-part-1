b40b91a4cb1f02dc2e2120251b5a018a4048ca75
kensam1 <samarakodykennedy@gmail.com>
2019-11-18 21:18:41 -0500

    My first commit. Did not finish, up to part 20

---
+++ GrailDairy/GrailDairy/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDairy
+//
+//  Created by Kennedy Samarakody on 11/18/19.
+//  Copyright © 2019 Kennedy Samarakody. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
