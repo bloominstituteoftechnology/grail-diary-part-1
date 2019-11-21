891f5eee23ea295b89735fc60455c252f0ea4faa
Jeremy Taylor <jtaylor@jctaylor.org>
2019-11-21 10:07:03 -0600

    Added files and connected view controllers to storyboard

---
+++ GrailDairy/GrailDairy/Models/POI.swift
@@ -0,0 +1,9 @@
+//
+//  POI.swift
+//  GrailDairy
+//
+//  Created by Jeremy Taylor on 11/21/19.
+//  Copyright © 2019 Bytes-Random L.L.C. All rights reserved.
+//
+
+import Foundation
---
+++ GrailDairy/GrailDairy/Views/POITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  POITableViewCell.swift
+//  GrailDairy
+//
+//  Created by Jeremy Taylor on 11/21/19.
+//  Copyright © 2019 Bytes-Random L.L.C. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
