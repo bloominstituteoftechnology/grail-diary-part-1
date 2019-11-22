34baf59f612dadaf048d6e7a158222b7792bed88
David <davidtwright89@gmail.com>
2019-11-21 21:40:40 -0500

    Added new files and set up POI struct

---
+++ GrailDiary/GrailDiary/Views/POITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  POITableViewCell.swift
+//  GrailDiary
+//
+//  Created by David Wright on 11/21/19.
+//  Copyright © 2019 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
