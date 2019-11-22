60461cbae0843ac3aec86d2958e1605a7f6a4af8
sal562 <salmanamer@gmail.com>
2019-11-21 19:38:14 -0800

    Start Part 2

---
+++ GrailDairy/GrailDairy/Model/POI.swift
@@ -0,0 +1,15 @@
+//
+//  POI.swift
+//  GrailDairy
+//
+//  Created by LambdaSchoolVM_Catalina on 11/21/19.
+//  Copyright © 2019 sierra. All rights reserved.
+//
+
+import UIKit
+
+struct POI {
+    var location: String
+    var country: String
+    var clues: [String]
+}
---
+++ GrailDairy/GrailDairy/View Controllers/POIsTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  POIsTableViewController.swift
+//  GrailDairy
+//
+//  Created by LambdaSchoolVM_Catalina on 11/21/19.
+//  Copyright © 2019 sierra. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
