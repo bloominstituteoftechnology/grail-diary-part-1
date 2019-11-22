51cee9697b04bf90d6efc171ca86bb3b1e48f6db
Christopher Price <christopher.r.price@gmail.com>
2019-11-21 23:34:55 -0500

    Got to number 17, but I am not sure if I have completed number 10 (data source methods implementation.

---
+++ GrailDiaryPart1/GrailDiaryPart1/Models/POI.swift
@@ -0,0 +1,15 @@
+//
+//  POI.swift
+//  GrailDiaryPart1
+//
+//  Created by Chris Price on 11/21/19.
+//  Copyright © 2019 Chris Price. All rights reserved.
+//
+
+import Foundation
+
+struct POI {
+    var location: String
+    var country: String
+    var clues: [String]
+}
