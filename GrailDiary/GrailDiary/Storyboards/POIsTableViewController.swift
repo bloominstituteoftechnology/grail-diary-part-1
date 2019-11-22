429b1b9f8f1ef2c1fbe26178c1c7a7de32a564e8
Joshua Rutkowski <jr.rtkski@gmail.com>
2019-11-21 19:35:03 -0500

    Set up POI struct

---
+++ GrailDiary/GrailDiary/Storyboards/POIsTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  POIsTableViewController.swift
+//  GrailDiary
+//
+//  Created by Joshua Rutkowski on 11/21/19.
+//  Copyright © 2019 Joshua Rutkowski. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GrailDiary/GrailDiary/View Controllers/POI.swift
@@ -0,0 +1,15 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Joshua Rutkowski on 11/21/19.
+//  Copyright © 2019 Joshua Rutkowski. All rights reserved.
+//
+
+import Foundation
+
+struct POI {
+    let location: String
+    let country: String
+    var clues: [String]
+}
