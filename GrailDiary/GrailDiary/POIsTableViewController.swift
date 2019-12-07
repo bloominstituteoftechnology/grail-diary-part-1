37c82491e578c5374e7bcbc5d65334dd13535a90
Kenny Dubroff <kennydubroff@hazystudios.com>
2019-12-07 10:57:49 -0800

    Embedded POIsTableViewController in Nav Controller

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  POIsTableViewController.swift
+//  GrailDiary
+//
+//  Created by Kenny on 12/7/19.
+//  Copyright © 2019 Hazy Studios. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
