97667e48d01f8cbdad6727cfe77f435297e9df16
Michael McGrath <maybemichael@icloud.com>
2019-12-10 15:07:21 -0800

    Auto Layout Afternoon Proj Finished

---
+++ GrailDiary/GrailDairy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDairy
+//
+//  Created by Michael on 12/10/19.
+//  Copyright © 2019 Michael. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
