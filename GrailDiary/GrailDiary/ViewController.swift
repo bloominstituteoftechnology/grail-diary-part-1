0012da4fd6bff9e8d38e65bff3f82cd777241353
Jorge Alvarez <jorgelalvarez52@gmail.com>
2019-12-10 12:37:44 -0800

    created project

---
+++ GrailDiary/GrailDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Jorge Alvarez on 12/10/19.
+//  Copyright © 2019 Jorge Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
