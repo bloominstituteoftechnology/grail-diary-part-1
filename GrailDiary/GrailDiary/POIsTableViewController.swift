3ee83b216b42225e90348a2100607d8095f5f737
Jorge Alvarez <jorgelalvarez52@gmail.com>
2019-12-12 15:50:06 -0800

    finished steps 7-11?

--- GrailDiary/GrailDiary/POIsTableViewController.swift
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class POIsTableViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let poiArray: [POI] = []
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 11 Done?
+        //tableView.delegate = self
+        //tableView.dataSource = self
     }
 
 
 }
+
+//        10. NOT DONE NEEDS tableviewcell file first
+//extension POIsTableViewController: UITableViewDataSource, UITableViewDelegate {
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return poiArray.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        //guard let cell = tableView.dequeueReusableCell(withIdentifier: "POICell", for: indexPath) as? POITableViewCell else {fatalError()}
+//
+//        //let poi = poiArray[indexPath.row]
+//        //cell.poi = poi //?
+          // return cell //?
+//
+//    }
+//
+//}
 
