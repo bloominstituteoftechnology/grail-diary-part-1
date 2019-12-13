485b0526a883771a454de9588b844f42081aa582
Jorge Alvarez <jorgelalvarez52@gmail.com>
2019-12-12 16:02:30 -0800

    finished 11-15

--- GrailDiary/GrailDiary/AddPOIViewController.swift
+++ GrailDiary/GrailDiary/AddPOIViewController.swift
@@ -8,23 +8,43 @@
 
 import UIKit
 
+// 13 done
+protocol AddPOIDelegate {
+    func poiWasAdded(_ poi: POI)
+}
+
 class AddPOIViewController: UIViewController {
+    
+    // 12 done
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var clue1TextField: UITextField!
+    @IBOutlet weak var clue2TextField: UITextField!
+    @IBOutlet weak var clue3TextField: UITextField!
+    
+    // 15 done
+    @IBAction func cancelTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func saveTapped(_ sender: Any) {
+    }
+    
+    // 14 done
+    var delegate: AddPOIDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+//extension AddFriendViewController: UITextFieldDelegate {
+//
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        // dismisses the keyboard
+//        textField.resignFirstResponder()
+//        return true
+//    }
+//}
