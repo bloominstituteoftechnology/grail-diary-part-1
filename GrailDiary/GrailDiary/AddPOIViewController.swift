c3a00d5540736bc609fd5ba02bca7e0faa54857d
Jorge Alvarez <jorgelalvarez52@gmail.com>
2019-12-12 16:58:19 -0800

    finished 15-20

--- GrailDiary/GrailDiary/AddPOIViewController.swift
+++ GrailDiary/GrailDiary/AddPOIViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 // 13 done
 protocol AddPOIDelegate {
-    func poiWasAdded(_ poi: POI)
+    func poiWasAdded(poi: POI) // used to be (_ poi: POI)
 }
 
 class AddPOIViewController: UIViewController {
@@ -23,11 +23,33 @@ class AddPOIViewController: UIViewController {
     @IBOutlet weak var clue3TextField: UITextField!
     
     // 15 done
+    // 16 done
     @IBAction func cancelTapped(_ sender: Any) {
-        
+        dismiss(animated: true, completion: nil)
     }
     
+    // 17, 18 done
     @IBAction func saveTapped(_ sender: Any) {
+        
+        guard let location = locationTextField.text, let country = countryTextField.text, !location.isEmpty, !country.isEmpty else {return}
+        
+        var poi = POI(location: location, country: country, clues: [])
+        
+        if let clue1 = clue1TextField.text, !clue1.isEmpty {
+            poi.clues.append(clue1)
+        }
+        
+        if let clue2 = clue2TextField.text, !clue2.isEmpty {
+            poi.clues.append(clue2)
+        }
+        
+        if let clue3 = clue3TextField.text, !clue3.isEmpty {
+            poi.clues.append(clue3)
+        }
+        
+        delegate?.poiWasAdded(poi: poi)
+        
+        dismiss(animated: true, completion: nil)
     }
     
     // 14 done
@@ -35,16 +57,29 @@ class AddPOIViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // 20 done ?"in the storyboard, for each textfield in this view, connect the delegate property of the textfield to this class"?
+        locationTextField.delegate = self
+        countryTextField.delegate = self
+        clue1TextField.delegate = self
+        clue2TextField.delegate = self
+        clue3TextField.delegate = self
     }
 
 }
 
-//extension AddFriendViewController: UITextFieldDelegate {
-//
-//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//        // dismisses the keyboard
-//        textField.resignFirstResponder()
-//        return true
-//    }
-//}
+// 19 done
+extension AddPOIViewController: UITextFieldDelegate {
+
+    // 21 NOT DONE
+     /*
+     unwrap the text and make sure it's not empty, then switch off the textfield to determine which one called this method;
+     change the firstResponder status to the appropriate textfield
+    */
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // dismisses the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+}
