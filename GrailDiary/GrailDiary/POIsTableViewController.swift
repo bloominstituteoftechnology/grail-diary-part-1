214d77f96a1792ef088f8357a393c14271f77652
Chris Gonzales <57194564+supergonzo812@users.noreply.github.com>
2020-01-21 12:35:33 -0800

    Completed Storyboard

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Chris Gonzales on 1/21/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
