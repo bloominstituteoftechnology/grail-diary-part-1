b1ad33e0e01451afe30f1a245b6e9dbb3f6f69c5
Keri Levesque <kerianne116@gmail.com>
2020-01-21 17:21:55 -0500

    working on add New POI View

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Keri Levesque on 1/21/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
