4ea6de0ed32980da6c94ad321ad889b1ff9cb7cb
Rebecca <mythreldrows@aol.com>
2020-01-21 17:29:07 -0500

    Everything until add New POI View

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Lambda_School_Loaner_254 on 1/21/20.
+//  Copyright © 2020 Rebecca Roshon. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    navigationController?.navigationBar.prefersLargeTitles = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
