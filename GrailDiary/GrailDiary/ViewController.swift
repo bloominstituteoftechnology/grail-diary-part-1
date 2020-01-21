73eaf832544114ddbe6e3689630cbf642baa1ffd
Samuel Esserman <samessermanlambda@gmail.com>
2020-01-21 14:59:54 -0500

    First commit

---
+++ GrailDiary/GrailDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Samuel Esserman on 1/21/20.
+//  Copyright © 2020 Samuel Esserman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
