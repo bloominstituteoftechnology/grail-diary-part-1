c0a08db8555b32c55a89fa94bc2a3034bedd0826
Nichole Davidson <nicholedavid@gmail.com>
2020-01-21 12:38:30 -0700

    Added Begining Phase

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Nichole Davidson on 1/21/20.
+//  Copyright © 2020 Nichole Davidson. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
