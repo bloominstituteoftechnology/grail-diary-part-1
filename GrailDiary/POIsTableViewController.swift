3bccce613e6ea0a3e379afac3bf1ef7443f6f8d1
Scott Harris <scottharris86@me.com>
2020-01-21 19:44:20 -0500

    completed all steps

---
+++ GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  POIsTableViewController.swift
+//  GrailDiary
+//
+//  Created by scott harris on 1/21/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UITableViewController {
+    
+}
