629b28f422b94e9ea06115ee122702d9819ac24e
Chris Gonzales <57194564+supergonzo812@users.noreply.github.com>
2020-01-23 09:02:28 -0800

    Project complete up to day 2 number 11

---
+++ GrailDiary/GrailDiary/POI.swift
@@ -0,0 +1,17 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Chris Gonzales on 1/23/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+import Foundation
+
+
+struct POI {
+    let location: String
+    let country: String
+    let cluse: [String]
+    
+}
--- GrailDiary/GrailDiary/POIsTableViewController.swift
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -8,8 +8,32 @@
 
 import UIKit
 
+extension POIsTableViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "POITableViewCell", for: indexPath)
+        return cell
+    }
+    
+    
+    
+}
+
 class POIsTableViewController: UIViewController {
 
+    let pOIs: [POI] = []
+    
+    @IBOutlet weak var pOITableView: UITableView!
+    
+
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
