2770056f587294dade99b45cb1555e9cadc73a87
Samuel Esserman <samessermanlambda@gmail.com>
2020-01-23 18:32:17 -0500

    Second commit

--- GrailDiary/GrailDiary/POIsTableViewController.swift
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class POIsTableViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var poiModels: [POI] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,4 +20,18 @@ class POIsTableViewController: UIViewController {
 
 
 }
-
+// QUESTION 9-10: EXTENSION
+extension POIsTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return poiModels.count
+    }
+    // FIRST THING: CREATE THE CELL.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "POICell", for: indexPath) as? POITableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+    
+    
+}
