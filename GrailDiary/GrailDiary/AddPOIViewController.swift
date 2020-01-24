5dd9ba6661bb9601923e453aa46a029ebcdb712c
Samuel Esserman <samessermanlambda@gmail.com>
2020-01-23 19:39:13 -0500

    Third commit

--- GrailDiary/GrailDiary/AddPOIViewController.swift
+++ GrailDiary/GrailDiary/AddPOIViewController.swift
@@ -5,15 +5,53 @@
 //  Created by Samuel Esserman on 1/23/20.
 //  Copyright © 2020 Samuel Esserman. All rights reserved.
 //
-
+// STEP 11
 import UIKit
+// STEP 12-13
+protocol AddPOIDelegate {
+    func poiWasAdded(_ poi: POI)
+}
 
 class AddPOIViewController: UIViewController {
 
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var clue1TextField: UITextField!
+    @IBOutlet weak var clue2TextField: UITextField!
+    @IBOutlet weak var clue3TextField: UITextField!
+    // STEP 14
+    var delegate: AddPOIDelegate?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    // STEP 15
+    // STEP 16 DISMISS
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    // STEP 17
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let location = locationTextField.text,
+            let country = countryTextField.text,
+            !location.isEmpty,
+            !country.isEmpty else { return }
+        var poi = POI (location: location, country: country, clues: [])
+        if let clue1 = clue1TextField.text,
+            !clue1.isEmpty {
+            poi.clues.append(clue1)
+        }
+        if let clue2 = clue2TextField.text,
+            !clue2.isEmpty {
+            poi.clues.append(clue2)
+        }
+        if let clue3 = clue3TextField.text,
+            !clue3.isEmpty {
+            poi.clues.append(clue3)
+        }
     }
     
 
