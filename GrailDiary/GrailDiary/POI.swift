cce122aa1c721cc6b4d1d611551ce4b6c77373cb
Enrique Gongora <EGTech24@gmail.com>
2020-01-23 21:21:31 -0800

    Completed Grail Diary Project

---
+++ GrailDiary/GrailDiary/POI.swift
@@ -0,0 +1,15 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Enrique Gongora on 1/23/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+import Foundation
+
+struct POI {
+    let location: String
+    let country: String
+    let clues: [String]
+}
