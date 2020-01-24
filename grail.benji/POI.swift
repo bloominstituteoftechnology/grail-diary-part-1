a55175f4b96986f26035fd6514aaea01d4195ad6
Benji Benjamin <benji@benglobal.us>
2020-01-24 08:49:20 -0600

    f

---
+++ grail.benji/POI.swift
@@ -0,0 +1,16 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Lambda_School_Losaner_256 on 1/23/20.
+//  Copyright © 2020 Benglobal Creative. All rights reserved.
+//
+
+import Foundation
+
+struct POI {
+    var location: String
+    var country: String
+    var clues: [String]
+    
+}
---
+++ grail.benji/POITableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  POITableViewCell.swift
+//  GrailDiary
+//
+//  Created by Lambda_School_Losaner_256 on 1/23/20.
+//  Copyright © 2020 Benglobal Creative. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
