7e207a98d7ceb3e91a70389034925fdcf6ca4ffc
Scott Harris <scottharris86@me.com>
2020-01-23 21:10:05 -0500

    completed all steps app works

---
+++ GrailDiary/AddPOIViewController.swift
@@ -0,0 +1,96 @@
+//
+//  AddPOIViewController.swift
+//  GrailDiary
+//
+//  Created by scott harris on 1/23/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+protocol AddPOIDelegate {
+    func poiWasAdded(_ poi: POI)
+}
+
+class AddPOIViewController: UIViewController {
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var clue1TextField: UITextField!
+    @IBOutlet weak var clue2TextField: UITextField!
+    @IBOutlet weak var clue3TextField: UITextField!
+    
+    var delegate: AddPOIDelegate?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancelTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveTapped(_ sender: Any) {
+        guard let location = locationTextField.text,
+            let country = countryTextField.text else { return }
+        
+        var poi = POI(location: location, country: country)
+        
+        if let text = clue1TextField.text {
+            poi.clues.append(text)
+            
+        }
+        
+        if let text = clue2TextField.text {
+            poi.clues.append(text)
+            
+        }
+        
+        if let text = clue3TextField.text {
+            poi.clues.append(text)
+            
+        }
+        
+        delegate?.poiWasAdded(poi)
+        
+        dismiss(animated: true, completion: nil)
+        
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension AddPOIViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard let text = textField.text,
+            !text.isEmpty else { return false }
+        
+        switch textField {
+            case locationTextField:
+//                return locationTextField.resignFirstResponder()
+                return countryTextField.becomeFirstResponder()
+            case countryTextField:
+                return clue1TextField.becomeFirstResponder()
+            case clue1TextField:
+                return clue2TextField.becomeFirstResponder()
+            case clue2TextField:
+                return clue3TextField.becomeFirstResponder()
+            case clue3TextField:
+                return clue3TextField.resignFirstResponder()
+            default:
+                return false
+        }
+        
+    }
+}
--- GrailDiary/POIsTableViewController.swift
+++ GrailDiary/POIsTableViewController.swift
@@ -8,6 +8,53 @@
 
 import UIKit
 
-class POIsTableViewController: UITableViewController {
+class POIsTableViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
     
+    var pois: [POI] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "AddPOIModalSegue" {
+            if let addPOIVC = segue.destination as? AddPOIViewController {
+                addPOIVC.delegate = self
+            }
+        } else if segue.identifier == "ShowPOIDetailSegue" { 
+            if let selectedRow = tableView.indexPathForSelectedRow,
+                let detailVC = segue.destination as? POIDetailViewController {
+                detailVC.poi = pois[selectedRow.row]
+            }
+        }
+    }
+
+    
+}
+
+extension POIsTableViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pois.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "POICell", for: indexPath) as? POITableViewCell else { return UITableViewCell() }
+        
+        let poi = pois[indexPath.row]
+        cell.poi = poi
+        
+        return cell
+    }
+    
+}
+
+extension POIsTableViewController: AddPOIDelegate {
+    func poiWasAdded(_ poi: POI) {
+        pois.append(poi)
+        tableView.reloadData()
+    }
 }
