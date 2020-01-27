684429f2fecaae312a0d4fa1399800329b9d75a2
Jarren Campos <jarrenmcampos@gmail.com>
2020-01-26 18:05:31 -0800

    Started Project

---
+++ GrailDiary/GrailDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Jarren Campos on 1/26/20.
+//  Copyright © 2020 Jarren Campos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
