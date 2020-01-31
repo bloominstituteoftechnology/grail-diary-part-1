6ceeb665e95219da9a373f496f226d2a467f7543
Christopher DeVito <toolofjesus@protonmail.com>
2020-01-31 00:35:31 -0500

    Set up Grail Diary layout up to step 15 of instructions.

---
+++ GrailDiary/GrailDiary/View Controllers/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Lambda_School_Loaner_259 on 1/30/20.
+//  Copyright © 2020 DeVitoC. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
