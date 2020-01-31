17b34236b7f4603c7767336eaa7ed16e6c4e5e18
David Williams <burmesedragon@att.net>
2020-01-31 15:56:15 -0800

    First Commit with view controller scenes done

---
+++ GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  POIsTableViewController.swift
+//  GrailDiary
+//
+//  Created by David Williams on 1/30/20.
+//  Copyright © 2020 david williams. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+// just for the commit
+    func Wat() {
+        print("hey")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
