83c9f8fe03648a132491df5228b1ee9224559699
Chad Parker <chad@chadparker.com>
2020-01-31 19:57:43 -0800

    Move project partway through

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  POIsTableViewController.swift
+//  GrailDiary
+//
+//  Created by Chad Parker on 1/31/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
