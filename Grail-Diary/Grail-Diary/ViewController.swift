e7bd9a11f5610e831365277a26e41acde108063e
Breena Greek <bgreek2019@gmail.com>
2020-01-31 23:17:33 -0500

    Initial Commit

---
+++ Grail-Diary/Grail-Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grail-Diary
+//
+//  Created by Breena Greek on 1/31/20.
+//  Copyright © 2020 Breena Greek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
