7e7cf768e62ef4d2239ece81b364259b7ebd562b
Libby Thomas <thomaslibby05@gmail.com>
2020-01-31 21:26:21 -0600

    Finished Storyboard

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  POIsTableViewController.swift
+//  GrailDiary
+//
+//  Created by Elizabeth Thomas on 1/30/20.
+//  Copyright © 2020 Libby Thomas. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
