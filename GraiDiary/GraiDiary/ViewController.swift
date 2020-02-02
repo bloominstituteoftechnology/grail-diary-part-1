089a7ad6cc722791f758be7687c139b19ac461f9
Jorge Talavera <jorgetalavera02@gmail.com>
2020-02-02 10:22:24 -0800

    Added a var to test project on GIT

---
+++ GraiDiary/GraiDiary/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GraiDiary
+//
+//  Created by Jorge Talavera on 2/2/20.
+//  Copyright © 2020 Jorge Talavera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    let name = "Jorge"
+    
+    
+}
+
