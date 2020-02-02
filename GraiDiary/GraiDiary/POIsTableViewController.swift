71b4af44740a6ed625d83f78044068cca4c72c76
Jorge Talavera <jorgetalavera02@gmail.com>
2020-02-02 12:53:23 -0800

    Finished all the way to adding constraints

--- GraiDiary/GraiDiary/POIsTableViewController.swift
+++ GraiDiary/GraiDiary/POIsTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  POIsTableViewController.swift
 //  GraiDiary
 //
 //  Created by Jorge Talavera on 2/2/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class POIsTableViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
