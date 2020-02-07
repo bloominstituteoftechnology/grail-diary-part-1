1dee542df7284dbc1b3a0d3e2d2d149af243d96d
Claudia Maciel <clc_80@yahoo.com>
2020-02-06 19:22:42 -0800

    POIDetailViewController coded

--- GrailDiary/GrailDiary/View Controller/POIDetailViewController.swift
+++ GrailDiary/GrailDiary/View Controller/POIDetailViewController.swift
@@ -15,21 +15,18 @@ class POIDetailViewController: UIViewController {
     @IBOutlet var countryLabel: UILabel!
     @IBOutlet var cluesTextView: UITextView!
     
+    var poi: POI?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func updateViews() {
+        guard let poi = poi else { return }
+        
+        locationLabel.text = poi.location
+        countryLabel.text = poi.country
+       // cluesTextView.text = poi.clues
     }
-    */
-
 }
