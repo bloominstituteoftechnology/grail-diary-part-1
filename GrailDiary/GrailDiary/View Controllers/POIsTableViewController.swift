e47b34f3b4242cc2d3225e2469f4fbb1a807d19e
Chad Parker <chad@chadparker.com>
2020-02-06 20:17:50 -0800

    Implement TableView methods

--- GrailDiary/GrailDiary/View Controllers/POIsTableViewController.swift
+++ GrailDiary/GrailDiary/View Controllers/POIsTableViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class POIsTableViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
+    var pointsOfInterest: [POI] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tableView.dataSource = self
     }
-
-
 }
 
+extension POIsTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pointsOfInterest.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "POICell", for: indexPath) as! POITableViewCell
+        let poi = pointsOfInterest[indexPath.row]
+        cell.poi = poi
+        return cell
+    }
+}
