6bd3aa411cc1ec98f2fa4b7cada5dd8d382c7fe7
David Williams <burmesedragon@att.net>
2020-02-06 21:09:44 -0800

    FINAL project, with added color and a surprise extra button and textField for one more clue element

---
+++ GrailDiary/Model/POI.swift
@@ -0,0 +1,15 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by David Williams on 2/6/20.
+//  Copyright © 2020 david williams. All rights reserved.
+//
+
+import Foundation
+
+struct POI {
+    let location: String
+    let country: String
+    var clues: [String]
+}
