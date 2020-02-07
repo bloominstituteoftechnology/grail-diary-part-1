a059cf5eafa363dcc07701385228a5786580cba8
Chad Parker <chad@chadparker.com>
2020-02-06 20:46:53 -0800

    Initial POITableViewCell

--- GrailDiary/GrailDiary/View Controllers/POITableViewCell.swift
+++ GrailDiary/GrailDiary/View Controllers/POITableViewCell.swift
@@ -10,15 +10,15 @@ import UIKit
 
 class POITableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    var poi: POI? {
+        didSet {
+            self.updateViews()
+        }
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    private func updateViews() {
+        guard let poi = poi else { return }
 
-        // Configure the view for the selected state
+        // TODO: update UI
     }
-
 }
