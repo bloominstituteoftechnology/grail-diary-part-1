6703052a477b7a5f5ed1d6a0ee6547bf2c8b0b7d
Chad Parker <chad@chadparker.com>
2020-02-09 16:35:29 -0800

    Detail: create poi property and updateViews()

--- GrailDiary/GrailDiary/View Controllers/POIDetailViewController.swift
+++ GrailDiary/GrailDiary/View Controllers/POIDetailViewController.swift
@@ -13,9 +13,23 @@ class POIDetailViewController: UIViewController {
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var countryLabel: UILabel!
     @IBOutlet weak var cluesTextView: UITextView!
-    
+
+    var poi: POI?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
+    }
+
+    private func updateViews() {
+        guard let poi = poi else { return }
+        locationLabel.text = poi.location
+        countryLabel.text = poi.country
+        var clueText = ""
+        for clue in poi.clues {
+            clueText.append("\(clue)\n")
+        }
+        cluesTextView.text = clueText
     }
     
 
