b1f4af39602a191c530723b22417238e573c3c96
Samuel Esserman <samessermanlambda@gmail.com>
2020-02-18 14:57:29 -0500

    First commit, all done up to question 37

---
+++ grail-diary-part-1/grail-diary-part-1/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  POIsTableViewController.swift
+//  grail-diary-part-1
+//
+//  Created by Samuel Esserman on 2/18/20.
+//  Copyright © 2020 Samuel Esserman. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
