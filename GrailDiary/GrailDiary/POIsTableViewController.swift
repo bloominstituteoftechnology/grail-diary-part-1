647ef4d594e8a7e78c7121971c7dc359f333519f
fthmls <fthmls.one@gmail.com>
2020-02-18 14:44:22 -0500

    Complete assignment

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by fthmls on 2/18/20.
+//  Copyright © 2020 fthmls. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
