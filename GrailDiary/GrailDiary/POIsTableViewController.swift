a0b9055f9ffee4ab17624839fe944ee43d250314
Hector L <hectoremil21@hotmail.com>
2020-02-18 14:11:26 -0500

    Oops

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Karen Rodriguez on 2/18/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
