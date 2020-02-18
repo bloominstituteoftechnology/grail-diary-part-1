eafdba358d0f7017f3b49fb82045b5a210880ef0
Shawn Gee <shawngee@gmail.com>
2020-02-18 06:08:44 -0500

    Part 1 completed

---
+++ GrailDiary/GrailDiary/POIsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  POIsViewController.swift
+//  GrailDiary
+//
+//  Created by Shawn Gee on 2/6/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import UIKit
+
+class POIsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
