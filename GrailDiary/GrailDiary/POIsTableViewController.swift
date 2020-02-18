9da4edeaf07f0d82f28df1e0df23a68ab3e25b52
Brad <braddiroff@icloud.com>
2020-02-18 15:38:51 -0500

    Adding layout and stack views halfway

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Bradley Diroff on 2/18/20.
+//  Copyright © 2020 Bradley Diroff. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
