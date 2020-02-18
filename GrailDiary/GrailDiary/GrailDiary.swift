f8139a4d7c99f15d13fec4e787b544a9c6a955b1
Wyatt Harrell <wyattaharrell@gmail.com>
2020-02-18 15:20:54 -0500

    setup first view controller

---
+++ GrailDiary/GrailDiary/GrailDiary.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Wyatt Harrell on 2/18/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+import UIKit
+
+class GrailDiary: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
