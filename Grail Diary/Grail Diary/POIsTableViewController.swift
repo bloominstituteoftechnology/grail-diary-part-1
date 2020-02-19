eec6bc002a5c6fa0795fcdb536c30cf2b91088c0
Gregory Omoma <omomagreg@yahoo.com>
2020-02-18 19:19:35 -0500

    finihed project of part 1

---
+++ Grail Diary/Grail Diary/POIsTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  POIsTableViewController.swift
+//  Grail Diary
+//
+//  Created by Gregory Omoma on 2/18/20.
+//  Copyright © 2020 Gregory Omoma. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
