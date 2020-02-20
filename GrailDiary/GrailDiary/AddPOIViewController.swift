dd0782b18ed10776f52df436162faf0622c3f6d1
Hector L <hectoremil21@hotmail.com>
2020-02-20 16:15:08 -0500

    Added outlet for all 5 text fields from New POI view to AddPOIViewController

--- GrailDiary/GrailDiary/AddPOIViewController.swift
+++ GrailDiary/GrailDiary/AddPOIViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class AddPOIViewController: UIViewController {
 
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var clue1TextField: UITextField!
+    @IBOutlet weak var clue2TextField: UITextField!
+    @IBOutlet weak var clue3TextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
