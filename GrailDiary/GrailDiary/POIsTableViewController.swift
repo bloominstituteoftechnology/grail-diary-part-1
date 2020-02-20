c1c7d179ffa7d8be3fbe26a9edb5b32fb361a366
Mark Gerrior <github@gerrior.com>
2020-02-20 13:26:41 -0800

    In POIsTableViewController.swift

--- GrailDiary/GrailDiary/POIsTableViewController.swift
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class POIsTableViewController: UIViewController {
-
+    var poi: [POI] = [POI(location: "Aulani",
+                          country: "US",
+                          clues: ["Hawaii", "Disney"])]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+
+extension POIsTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        poi.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "POICell") as? POITableViewCell else { return UITableViewCell() }
+        
+        return cell
+    }
+}
