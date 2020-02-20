cda3038d72585cf4e2167a7e87005b8a56f4bfa7
Mark Gerrior <github@gerrior.com>
2020-02-20 15:37:52 -0800

    Step 33

--- GrailDiary/GrailDiary/POITableViewCell.swift
+++ GrailDiary/GrailDiary/POITableViewCell.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class POITableViewCell: UITableViewCell {
-
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var cluesCountLabel: UILabel!
+    
+    var poi: POI? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    func updateViews() {
+        guard let poi = poi else { return }
+        
+        locationLabel.text = poi.location
+        countryLabel.text = poi.country
+        cluesCountLabel.text = "\(poi.clues.count) clues"
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
