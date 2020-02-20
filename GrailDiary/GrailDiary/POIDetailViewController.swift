ce5399703cd3726423eb99ae1312c43df3ee584c
Mark Gerrior <github@gerrior.com>
2020-02-20 15:44:12 -0800

    Step 38

--- GrailDiary/GrailDiary/POIDetailViewController.swift
+++ GrailDiary/GrailDiary/POIDetailViewController.swift
@@ -10,10 +10,31 @@ import UIKit
 
 class POIDetailViewController: UIViewController {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var cluesTextView: UITextView!
+    @IBOutlet weak var countryLabel: UILabel!
+    
+    var poi: POI?
+    
+    private func updateViews() {
+        guard let poi = poi else { return }
+        
+        locationLabel.text = poi.location
+        countryLabel.text = poi.country
+        
+        var cluesText = ""
+        for clue in poi.clues {
+            cluesText += clue + "\r"
+        }
+        
+        cluesTextView.text = cluesText
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        updateViews()
     }
 
     // MARK: - Navigation
