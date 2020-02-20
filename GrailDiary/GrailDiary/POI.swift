f09925e4edc29d366e409875e36ab5bc8a89c1ec
Mark Gerrior <github@gerrior.com>
2020-02-20 13:07:08 -0800

    POI.swift

---
+++ GrailDiary/GrailDiary/POI.swift
@@ -0,0 +1,15 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Mark Gerrior on 2/20/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import Foundation
+
+struct POI {
+    var location: String
+    var country: String
+    var clues: [String]
+}
