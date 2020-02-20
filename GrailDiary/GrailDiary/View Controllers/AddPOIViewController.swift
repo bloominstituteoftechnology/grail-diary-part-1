af8e880f8780cf25890671a95ad8f8b5936c2f93
Diego Covarrubias <dcov@pm.me>
2020-02-20 14:13:51 -0800

    Implemented AddPOIViewController functionality

--- GrailDiary/GrailDiary/View Controllers/AddPOIViewController.swift
+++ GrailDiary/GrailDiary/View Controllers/AddPOIViewController.swift
@@ -8,23 +8,74 @@
 
 import UIKit
 
+protocol AddPOIDelegate {
+    
+    func poiWasAdded(_ poi: POI)
+}
+
 class AddPOIViewController: UIViewController {
 
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var clueOneTextField: UITextField!
+    @IBOutlet weak var clueTwoTextField: UITextField!
+    @IBOutlet weak var clueThreeTextField: UITextField!
+    
+    var delegate: AddPOIDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
     }
-    */
+    
+    @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+        if let delegate = self.delegate {
+            guard let location = locationTextField.text else { return }
+            guard let country = countryTextField.text else { return }
+            
+            var clues = Array<String>()
+            func maybeAppendClue(_ textField: UITextField) {
+                if let clue = textField.text {
+                    clues.append(clue)
+                }
+            }
+            maybeAppendClue(clueOneTextField)
+            maybeAppendClue(clueTwoTextField)
+            maybeAppendClue(clueThreeTextField)
+            
+            delegate.poiWasAdded(POI(
+                location: location,
+                country: country,
+                clues: clues
+            ))
+        }
+    }
+}
 
+extension AddPOIViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.text != nil {
+            switch textField {
+            case locationTextField:
+                countryTextField.becomeFirstResponder()
+            case countryTextField:
+                clueOneTextField.becomeFirstResponder()
+            case clueOneTextField:
+                clueTwoTextField.becomeFirstResponder()
+            case clueTwoTextField:
+                clueThreeTextField.becomeFirstResponder()
+            default:
+                return true
+            }
+            return false
+        }
+        
+        return true
+    }
 }
