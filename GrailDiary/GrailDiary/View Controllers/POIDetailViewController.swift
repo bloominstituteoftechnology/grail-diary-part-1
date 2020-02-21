adb37a376bee4d69cbc1250973786b0f1899fe09
Harmony Radley <harmonyradley99@gmail.com>
2020-02-21 09:41:08 -0500

    3 steps left

--- GrailDiary/GrailDiary/View Controllers/POIDetailViewController.swift
+++ GrailDiary/GrailDiary/View Controllers/POIDetailViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class POIDetailViewController: UIViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViews.becomeFirstResponder()
+    }
+    
+    
     
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var countryLabel: UILabel!
-    @IBOutlet weak var cluesLabel: UILabel!
     @IBOutlet weak var cluesTextView: UITextView!
     
     var poi: POI
     
-    func updateViews() {
+    private func updateViews() {
+        guard let poi = poi else { return }
         
+        locationLabel.text = poi.location
+        countryLabel.text = poi.country
+        cluesTextView.text = "\(poi.clues.count) clues"
     }
-
 }
