ae30ecc6b3e89edc1444f214d23b0da7cd6bdd36
Nichole Davidson <nicholedavid@gmail.com>
2020-02-20 17:16:17 -0700

    Added IBOutlets and updateViews code on POIDetailViewController

--- GrailDiary/GrailDiary/POIDetailViewController.swift
+++ GrailDiary/GrailDiary/POIDetailViewController.swift
@@ -9,22 +9,39 @@
 import UIKit
 
 class POIDetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var cluesTextView: UITextView!
+    
+    var poi: POI?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       updateViews()
+    }
+    
+    private func updateViews() {
+        guard let poi = poi else { return }
+        
+        locationLabel.text = poi.location
+        countryLabel.text = poi.country
+        
+            
+        }
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
+
 
 }
