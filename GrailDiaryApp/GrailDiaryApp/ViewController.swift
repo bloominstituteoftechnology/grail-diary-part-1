3911b1de5efb3b489b0d719d5d8361ddfc31c845
Cameron Collins <cameroncollins19@yahoo.com>
2020-03-17 15:04:32 -0500

    Added Project

---
+++ GrailDiaryApp/GrailDiaryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiaryApp
+//
+//  Created by Cameron Collins on 3/17/20.
+//  Copyright © 2020 Cameron Collins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
