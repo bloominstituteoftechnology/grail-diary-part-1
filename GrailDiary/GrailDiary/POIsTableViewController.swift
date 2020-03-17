63d0b3b6321afef3572792d04588bec36e6ceb02
Nihal Erdal <nihalerdall@gmail.com>
2020-03-17 13:40:18 -0700

    completed List of Clues View

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Nihal Erdal on 3/17/20.
+//  Copyright © 2020 Nihal Erdal. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
