ec9078a3cef3bc33be17b531f8be11d4c490f954
Harmony Radley <harmonyradley99@gmail.com>
2020-03-17 16:43:29 -0400

    Added List of clue view, Add New POI View, Working on POI Detail View

---
+++ Grail Diary/Grail Diary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grail Diary
+//
+//  Created by Harmony Radley on 3/17/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
