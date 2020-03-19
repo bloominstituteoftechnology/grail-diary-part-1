1d8609415800cbe66399580e8ee92b1153f90703
Harmony Radley <harmonyradley99@gmail.com>
2020-03-19 16:17:09 -0400

    Followed instructions for steps 7. - 11.

--- Grail Diary/Grail Diary/View Controllers/POIsTableViewController.swift
+++ Grail Diary/Grail Diary/View Controllers/POIsTableViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class POIsTableViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    var pois = [POI]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
     }
 
 
 }
 
+// Outside of class
+
+extension POIsTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pois.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell =
+            tableView.dequeueReusableCell(withIdentifier:
+            "POICell", for: indexPath) as?
+            POITableViewCell else {
+                fatalError("Cell is not a POITableViewCell")
+        }
+        
+        let poi = pois[indexPath.row]
+        
+        
+        
+        
+        return cell
+    }
+}
