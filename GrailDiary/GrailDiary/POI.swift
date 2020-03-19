33b6f3699e6f8a580e448199e38284b8478b594e
Nihal Erdal <nihalerdall@gmail.com>
2020-03-19 13:05:55 -0700

    Added files

---
+++ GrailDiary/GrailDiary/POI.swift
@@ -0,0 +1,9 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Nihal Erdal on 3/19/20.
+//  Copyright © 2020 Nihal Erdal. All rights reserved.
+//
+
+import Foundation
---
+++ GrailDiary/GrailDiary/POITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  POITableViewCell.swift
+//  GrailDiary
+//
+//  Created by Nihal Erdal on 3/19/20.
+//  Copyright © 2020 Nihal Erdal. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
