43d22e00b99690a1f612bcb3450ae3abb2b98f95
Chris Dobek <dobek.christopher@gmail.com>
2020-03-19 18:46:19 -0400

    Almost Completed App

---
+++ GrailDiary/GrailDiary/POITableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  POITableViewCell.swift
+//  GrailDiary
+//
+//  Created by Chris Dobek on 3/19/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var cluesCountLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
--- GrailDiary/GrailDiary/POIsTableViewController.swift
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class POIsTableViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var pois: [POI] = [POI(location: "Charlotte", country: "USA", clues: ["Queen City"])]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "AddPOIModalSegue" {
+                    guard let newPoiVC = segue.destination as? AddPOIViewController else { return }
+                
+                newPoiVC.delegate = self
+            }
+        }
+    }
+    
+extension POIsTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pois.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "POICell", for: indexPath) as? POITableViewCell else {
+            fatalError("Cell is not of POITableViewCell")
+        }
+            return cell
+        }
+    }
 
+extension POIsTableViewController: AddPOIDelegate {
+    func poiWasAdded(poi: POI) {
+        pois.append(poi)
+        dismiss(animated: true, completion: nil)
+        tableView.reloadData()
+    }
+
+    
 }
-
