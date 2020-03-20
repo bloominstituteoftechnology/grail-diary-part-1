81fa460cd5fabf8ddace67bd6947966a86d2a649
Shawn-James <shawn.james@me.com>
2020-03-19 20:59:36 -0400

    add file tasks

---
+++ GrailDiary/GrailDiary/Model/POI.swift
@@ -0,0 +1,9 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Shawn James on 3/19/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+import Foundation
---
+++ GrailDiary/POITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  GrailDiary
+//
+//  Created by Shawn James on 3/19/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
