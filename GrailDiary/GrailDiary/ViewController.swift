a4f3842c06b9d08951b6adc009f09d435434a135
Aaron Peterson <aarondp2011@gmail.com>
2020-04-10 11:27:12 -0500

    Initial commit

---
+++ GrailDiary/GrailDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Aaron Peterson on 4/10/20.
+//  Copyright © 2020 Aaron Peterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
