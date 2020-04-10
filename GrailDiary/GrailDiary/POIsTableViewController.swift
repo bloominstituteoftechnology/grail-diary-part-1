ff1b894660ec899f38f1a006d9838e7d520defc4
Clayton Watkins <watkinclayton17@yahoo.com>
2020-04-10 00:37:06 -0700

    Finished project. (Should've commited more oops)

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Clayton Watkins on 4/9/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
