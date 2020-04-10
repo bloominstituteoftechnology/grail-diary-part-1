78db93f1ac3514095b6ff82f6d31055d43cccad6
Your Name Here <youremailhere>
2020-04-10 00:06:18 -0400

    Set up Project and initaliszed ViewController

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  POIsTableViewController.swift
+//  GrailDiary
+//
+//  Created by Sammy Alvarado on 4/9/20.
+//  Copyright © 2020 Sammy Alvarado. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
