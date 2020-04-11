5cdba0ab70ca82c65f505a50b801fa8dd6510c35
Kenneth Jones <kennethjones1991@outlook.com>
2020-04-10 19:38:02 -0600

    Completed Part 1 of Grail Diary

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  POIsTableViewController.swift
+//  GrailDiary
+//
+//  Created by Kenneth Jones on 4/10/20.
+//  Copyright © 2020 Kenneth Jones. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
