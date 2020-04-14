49f2ed85f741c8354d8893fca0bc0cc90ee1b499
violet-love <violetlove9419@icloud.com>
2020-04-14 13:16:22 -0700

    set up the list of clues view

---
+++ Grail Diary/Grail Diary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grail Diary
+//
+//  Created by Violet Lavender Love on 4/14/20.
+//  Copyright © 2020 Love. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
