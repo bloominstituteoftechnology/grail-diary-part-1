c7df634eb4969e1800038013800bb870ac6a869b
Enzo Jimenez-Soto <enzojimenezsoto@gmail>
2020-04-14 14:25:59 -0700

    Finsihed Part 1

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Enzo Jimenez-Soto on 4/14/20.
+//  Copyright © 2020 Enzo Jimenez-Soto. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
