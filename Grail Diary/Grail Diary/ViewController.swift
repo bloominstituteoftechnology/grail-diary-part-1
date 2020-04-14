d2d461a8027c39161e62330a50e9bc00a8ac6430
kelson99 <kelson99hartle@icloud.com>
2020-04-14 17:03:49 -0600

    I will finish my project and submit later due to strange error

---
+++ Grail Diary/Grail Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grail Diary
+//
+//  Created by Kelson Hartle on 4/14/20.
+//  Copyright © 2020 Kelson Hartle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
