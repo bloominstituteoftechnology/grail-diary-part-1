688d2a58c36c88387f32a5618501e868616d484a
Aaron Peterson <aarondp2011@gmail.com>
2020-04-16 10:41:25 -0500

    Created required swift files.

---
+++ GrailDiary/GrailDiary/POI.swift
@@ -0,0 +1,9 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Aaron Peterson on 4/16/20.
+//  Copyright © 2020 Aaron Peterson. All rights reserved.
+//
+
+import Foundation
---
+++ GrailDiary/GrailDiary/POITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  POITableViewCell.swift
+//  GrailDiary
+//
+//  Created by Aaron Peterson on 4/16/20.
+//  Copyright © 2020 Aaron Peterson. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
