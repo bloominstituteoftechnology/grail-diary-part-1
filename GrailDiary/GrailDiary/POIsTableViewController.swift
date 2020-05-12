32f5ab80088ef236e20b06757915b4b3b447e8ec
Miguel Marquez <marquez53095@outlook.com>
2020-05-12 10:51:11 -0700

    First revision, I renamed my ViewController to POIsTableViewController

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Miguel Marquez on 5/12/20.
+//  Copyright © 2020 Miguel Marquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
