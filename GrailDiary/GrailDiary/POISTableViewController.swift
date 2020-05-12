0d0112c6d2bc1b67e12de93399ee9695e0980f3a
Ian Becker <ian-becker@lambdastudents.com>
2020-05-12 16:21:10 -0700

    Forgot to have a swift file for subclass.

---
+++ GrailDiary/GrailDiary/POISTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  POISTableViewController.swift
+//  GrailDiary
+//
+//  Created by Ian Becker on 5/12/20.
+//  Copyright © 2020 Ian Becker. All rights reserved.
+//
+
+import UIKit
+
+class POISTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
