6533955e21973bf9087d0c0aec65e73d110692dd
violet-love <violetlove9419@icloud.com>
2020-05-12 11:06:33 -0700

    set xcode project

---
+++ GrailDiary/GrailDiary/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Violet Lavender Love on 5/12/20.
+//  Copyright © 2020 Love. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
