13c810c4ab03d064bd68fbd43e325d888f1ab2f0
Bryan Cress <8675309+cressbj@users.noreply.github.com>
2020-05-12 14:58:36 -0400

    uploaded project, read instructions

---
+++ GrailDiary/GrailDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Bryan Cress on 5/12/20.
+//  Copyright © 2020 Bryan Cress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
