ad3aefd9b71a727353ccbd1513c1ee7d1edd3126
Ian Becker <ian-becker@lambdastudents.com>
2020-05-14 15:58:37 -0700

    Gave outlet properties from 5 textfields, gave it a delegate, gave cancel and save actions, connected delegate property of textfields to class.

--- GrailDiary/GrailDiary/View Controllers/AddPOIViewController.swift
+++ GrailDiary/GrailDiary/View Controllers/AddPOIViewController.swift
@@ -8,15 +8,55 @@
 
 import UIKit
 
-class AddPOIViewController: UIViewController {
+protocol AddPOIDelegate {
+    func poiWasAdded(_ poi: POI)
+}
 
+class AddPOIViewController: UIViewController {
+    
+    @IBOutlet weak var locationVC: UITextField!
+    @IBOutlet weak var countryVC: UITextField!
+    @IBOutlet weak var clue1VC: UITextField!
+    @IBOutlet weak var clue2VC: UITextField!
+    @IBOutlet weak var clue3VC: UITextField!
+    
+    var delegate: AddPOIDelegate?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func save(_ sender: Any) {
+        
+        guard let location = locationVC.text, let country = countryVC.text else { return }
+        
+        var clues: [String] = []
+        
+        if let clue1 = clue1VC.text, clue1 != "" {
+            clues.append(clue1)
+        }
+        if let clue2 = clue2VC.text, clue2 != "" {
+            clues.append(clue2)
+        }
+        if let clue3 = clue3VC.text, clue3 != "" {
+        clues.append(clue3)
+        }
+        
+        let poi = POI(location: location, country: countryVC, clues: clues)
+        
+        delegate?.poiWasAdded(poi)
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -27,4 +67,10 @@ class AddPOIViewController: UIViewController {
     }
     */
 
+}
+
+extension AddPOIViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+    }
 }
