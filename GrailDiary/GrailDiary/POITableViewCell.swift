100b7786e408ceecdf494821d7978749a3ac9651
Miguel Marquez <marquez53095@outlook.com>
2020-05-15 05:07:19 -0700

    Revision 12, I've assigned these classes AddPOIViewController, POIDetailViewController, POITableViewCell, and POI to the correct View Control

--- GrailDiary/GrailDiary/POITableViewCell.swift
+++ GrailDiary/GrailDiary/POITableViewCell.swift
@@ -8,23 +8,17 @@
 
 import UIKit
 
-class POITableViewCell: UIViewController {
+class POITableViewCell: UITableViewCell {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
     }
-    
 
-    /*
-    // MARK: - Navigation
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // Configure the view for the selected state
     }
-    */
 
 }
