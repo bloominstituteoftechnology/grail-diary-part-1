0444140bd19988c95c3d6cb7871804a15ff9408a
johnmccants002 <johnmccants002@gmail.com>
2020-05-19 13:49:53 -0700

    Part 1 Done

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by John McCants on 5/19/20.
+//  Copyright © 2020 John McCants. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
