5dc6cc92c69c61c8510eb9221c95bf30cc6749fa
Gladymir <gaethan0815@gmail.com>
2020-05-22 23:51:46 -0600

    GrailDiary

---
+++ GrailDiary/GrailDiary/P0IsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Gladymir Philippe on 5/22/20.
+//  Copyright © 2020 Gladymir Philippe. All rights reserved.
+//
+
+import UIKit
+
+class P0IsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
