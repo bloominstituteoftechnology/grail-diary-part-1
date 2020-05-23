4f6183ac1f54fcd22554709840c6ee4abb0c3804
JamesSedlacek <jsedlacekjr@gmail.com>
2020-05-22 23:29:39 -0400

    Completed Project

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by James Sedlacek on 5/22/20.
+//  Copyright © 2020 James Sedlacek. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
