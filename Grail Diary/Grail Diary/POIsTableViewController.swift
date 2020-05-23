8d9bd3d6635db02f4ec6ca991db4208ac7bacff4
mcampbell34 <mcampbell@fastly.com>
2020-05-23 09:36:11 -0700

    SemiFinal

---
+++ Grail Diary/Grail Diary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grail Diary
+//
+//  Created by Marcus Campbell on 5/21/20.
+//  Copyright © 2020 Marcus Campbell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
