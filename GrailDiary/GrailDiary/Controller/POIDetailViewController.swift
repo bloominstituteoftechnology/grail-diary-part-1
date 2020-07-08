c863e9457924bbddaa4b3d28dfa0b7aaa7156d96
James McDougall <jameswiliammcdougall@gmail.com>
2020-07-08 13:03:02 -0500

    Added Grail Diary 2 files

---
+++ GrailDiary/GrailDiary/Controller/POIDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  POIDetailViewController.swift
+//  GrailDiary
+//
+//  Created by Lambda_School_loaner_226 on 7/8/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class POIDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GrailDiary/GrailDiary/Controller/POITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  POITableViewCell.swift
+//  GrailDiary
+//
+//  Created by Lambda_School_loaner_226 on 7/8/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
