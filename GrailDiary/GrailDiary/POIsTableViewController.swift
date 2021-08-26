64697eeff0bbaa70e66d3413d98287a9fdb129bb
Harmony <harmonymorse@icloud.com>
2021-08-26 12:11:43 -0700

    done with constraints

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Harmony Morse on 8/26/21.
+//  Copyright © 2021 Harmony Morse. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
